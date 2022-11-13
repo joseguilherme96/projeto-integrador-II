c05bd2e89411521374a82bedb8b3016a10782688
José Guilherme <jose.guilherme96@outlook.com.br>
2022-11-13 16:46:14 -0300

    Criado pagina inicial do portal

---
+++ index.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css">
+    <title>Home</title>
+</head>
+<body>
+    <div class="container">
+    <?php include "menu_temp.php"?>
+    <div class="row">
+      <div class="col-12">
+        <a href="index.php">Home</a> / 
+      </div>
+    </div>
+    <div class="row justify-content-center">
+    <div class="col-5 ">
+      <input type="text" class="form-control " placeholder="Pesquise sobre cosplay" />
+    </div>
+    <div class="col-2 p-2">
+      <i class="bi-search "></i>
+    </div>
+  </div>
+    <section class="cinema">
+        <div class="row text-center">
+            <div class="col-12 col-sm-12 col-lg-12">
+                <h1>CINEMA</h1>
+            </div>
+        </div>
+        <div class="row ">
+            <div class="col-12 col-sm-12 col-lg-12 ">
+                <p class="">
+                    Lorem ipsum dolor sit amet, consecteturLorem ipsum
+                    dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
+                    consecteturLorem ipsum dolor sit amet, consectetur
+                </p>
+            </div>
+            <div class=" col-sm-12 col-lg-12 text-center p-1">
+                <picture >
+                    <source srcset="https://via.placeholder.com/1700x320" class="img-fluid" media="(min-width:992px)">
+                    <source srcset="https://via.placeholder.com/821x229" class="img-fluid" media="(min-width:576px)">
+                    <img src="https://via.placeholder.com/299x119" class="img-fluid">
+                </picture>
+            </div>
+        </div>
+        <div class="row ">
+            <div class="col-6 col-sm-6 col-lg-6 p-1">
+                <img src="https://via.placeholder.com/600x350" class="img-fluid ">
+            </div>
+            <div class="col-6 col-sm-6 col-lg-6 p-1">
+                <p>
+                    Lorem ipsum dolor sit amet, consecteturLorem ipsum
+                    dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
+                    consecteturLorem ipsum dolor sit amet, consectetur
+                    Lorem ipsum dolor sit amet, consecteturLorem ipsum
+                    dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
+                    consecteturLorem ipsum dolor sit amet, consectetur
+                </p>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="row text-center">
+            <div class="col-12 col-sm-12 col-lg-12">
+                <h1>HQS</h1>
+            </div>
+        </div>
+        <div class="row ">
+            <div class="col-6 col-sm-6 col-lg-6 p-1">
+                <img src="https://via.placeholder.com/600x350" class="img-fluid ">
+            </div>
+            <div class="col-6 col-sm-6 col-lg-6 p-1">
+                <p>
+                    Lorem ipsum dolor sit amet, consecteturLorem ipsum
+                    dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
+                    consecteturLorem ipsum dolor sit amet, consectetur
+                    Lorem ipsum dolor sit amet, consecteturLorem ipsum
+                    dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
+                    consecteturLorem ipsum dolor sit amet, consectetur
+                </p>
+            </div>
+        </div>
+        <div class="row ">
+            <div class="col-12 col-sm-12 col-lg-12">
+                <p>
+                    Lorem ipsum dolor sit amet, consecteturLorem ipsum
+                    dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
+                    consecteturLorem ipsum dolor sit amet, consectetur
+                </p>
+            </div>
+            <div class=" col-sm-12 col-lg-12 text-center p-1">
+                <picture >
+                    <source srcset="https://via.placeholder.com/1700x320" class="img-fluid" media="(min-width:992px)">
+                    <source srcset="https://via.placeholder.com/821x229" class="img-fluid" media="(min-width:576px)">
+                    <img src="https://via.placeholder.com/299x119" class="img-fluid">
+                </picture>
+            </div>
+        </div>
+        <div class="col-12 col-sm-12 col-lg-12">
+                <p>
+                    Lorem ipsum dolor sit amet, consecteturLorem ipsum
+                    dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
+                    consecteturLorem ipsum dolor sit amet, consectetur
+                </p>
+        </div>
+    </section>
+    <section class="cosplay text-center">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-lg-12">
+                <h1>COSPLAY</h1>
+            </div>
+        </div>
+        <div class="row ">
+            <div class="col-12 col-sm-12 col-lg-12">
+                <p>
+                    Lorem ipsum dolor sit amet, consecteturLorem ipsum
+                    dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
+                    consecteturLorem ipsum dolor sit amet, consectetur
+                </p>
+            </div>
+            <div class=" col-sm-12 col-lg-12 text-center p-1">
+                <picture >
+                    <source srcset="https://via.placeholder.com/1700x320" class="img-fluid" media="(min-width:992px)">
+                    <source srcset="https://via.placeholder.com/821x229" class="img-fluid" media="(min-width:576px)">
+                    <img src="https://via.placeholder.com/299x119" class="img-fluid">
+                </picture>
+            </div>
+        </div>
+        <div class="row ">
+            <div class="col-6 col-sm-6 col-lg-6 p-1">
+                <img src="https://via.placeholder.com/600x350" class="img-fluid ">
+            </div>
+            <div class="col-6 col-sm-6 col-lg-6 p-1">
+                <p>
+                    Lorem ipsum dolor sit amet, consecteturLorem ipsum
+                    dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
+                    consecteturLorem ipsum dolor sit amet, consectetur
+                    Lorem ipsum dolor sit amet, consecteturLorem ipsum
+                    dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
+                    consecteturLorem ipsum dolor sit amet, consectetur
+                </p>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="row text-center">
+            <div class="col-12 col-sm-12 col-lg-12">
+                <h1>SERIES & TV</h1>
+            </div>
+        </div>
+        <div class="row ">
+            <div class="col-6 col-sm-6 col-lg-6 p-1">
+                <img src="https://via.placeholder.com/600x350" class="img-fluid ">
+            </div>
+            <div class="col-6 col-sm-6 col-lg-6 p-1">
+                <p>
+                    Lorem ipsum dolor sit amet, consecteturLorem ipsum
+                    dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
+                    consecteturLorem ipsum dolor sit amet, consectetur
+                    Lorem ipsum dolor sit amet, consecteturLorem ipsum
+                    dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
+                    consecteturLorem ipsum dolor sit amet, consectetur
+                </p>
+            </div>
+        </div>
+        <div class="row ">
+            <div class="col-12 col-sm-12 col-lg-12">
+                <p>
+                    Lorem ipsum dolor sit amet, consecteturLorem ipsum
+                    dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
+                    consecteturLorem ipsum dolor sit amet, consectetur
+                </p>
+            </div>
+            <div class=" col-sm-12 col-lg-12 text-center p-1">
+                <picture >
+                    <source srcset="https://via.placeholder.com/1700x320" class="img-fluid" media="(min-width:992px)">
+                    <source srcset="https://via.placeholder.com/821x229" class="img-fluid" media="(min-width:576px)">
+                    <img src="https://via.placeholder.com/299x119" class="img-fluid">
+                </picture>
+            </div>
+        </div>
+        <div class="col-12 col-sm-12 col-lg-12">
+                <p>
+                    Lorem ipsum dolor sit amet, consecteturLorem ipsum
+                    dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
+                    consecteturLorem ipsum dolor sit amet, consectetur
+                </p>
+        </div>
+    </section>
+</div>
+</body>
+</html>
