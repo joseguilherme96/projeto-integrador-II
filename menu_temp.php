ff657ac00dcaa81530b75bda24727523a0134e27
José Guilherme <jose.guilherme96@outlook.com.br>
2022-11-13 16:45:19 -0300

    Criado menu temporário até definição do menu definitivo

---
+++ menu_temp.php
@@ -0,0 +1,8 @@
+<h1>Menu temporário das paginas</h1>
+    <nav>
+        <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="wireframes-html/index.html">Wireframes</a></li>
+        <li><a href="cinema/index.php">Cinema</a></li>
+        </ul>
+</nav>
