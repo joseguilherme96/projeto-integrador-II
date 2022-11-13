1eaa008f746248800bbd11ac4eab223720910208
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2022-11-13 17:23:40 -0300

    novo

---
+++ nav.php
@@ -0,0 +1,85 @@
+
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+             <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+    
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Cinema
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Cinema</a></li>
+              <li><a class="dropdown-item" href="#">Literatura</a></li>
+              <li><a class="dropdown-item" href="#">Entrevista</a></li>
+              <li><a class="dropdown-item" href="#">Streaming</a></li>
+              <li><a class="dropdown-item" href="#">Filmes</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+           HQS
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Materias</a></li>
+              <li><a class="dropdown-item" href="#">Top Five</a></li>
+              <li><a class="dropdown-item" href="#">Criticas</a></li>
+              <li><a class="dropdown-item" href="#">Melhores criticas</a></li>
+              <li><a class="dropdown-item" href="#">Melhores autores</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+       Cosplay
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Cosplay</a></li>
+              <li><a class="dropdown-item" href="#">Animes</a></li>
+              <li><a class="dropdown-item" href="#">Criticas</a></li>
+              <li><a class="dropdown-item" href="#">Eventos</a></li>
+              <li><a class="dropdown-item" href="#">Games</a></li>
+              <li><a class="dropdown-item" href="#">Games</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+           Series e TV
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Serie/Tv</a></li>
+              <li><a class="dropdown-item" href="#">Resenhas</a></li>
+              <li><a class="dropdown-item" href="#">Lançamentos</a></li>
+              <li><a class="dropdown-item" href="#">Criticas</a></li>
+              <li><a class="dropdown-item" href="#">Autores</a></li>
+            </ul>
+          </li>
+        </ul>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">Quem somos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contatos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Normas de Utilização</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Autores</a>
+            </li>
+  
+      </div>
+    </div>
+  </nav>
