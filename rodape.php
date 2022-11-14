87d9425547d77ca303194e54d3e6b7417b4ab482
José Guilherme <jose.guilherme96@outlook.com.br>
2022-11-14 17:03:07 -0300

    Criado rodapé, importado para as paginas home e cinema, incluso link css do bootstrap a partir da pasta local para uso offline

---
+++ rodape.php
@@ -0,0 +1,81 @@
+
+        <div class="row border">
+                <div class="col-sm-3 col-lg-1">
+                    <ul>
+                        <li><a href="">Cinema</a></li>
+                        <li><a href="">Filmes</a></li>
+                        <li><a href="">Cinema</a></li>
+                        <li><a href="">Streaming</a></li>
+                        <li><a href="">Literatura</a></li>
+                        <li><a href="">Entrevistas</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3 col-lg-1 ">
+                    <ul >
+                        <li><a href="">HQS</a></li>
+                        <li><a href="">Matérias</a></li>
+                        <li><a href="">Top Five</a></li>
+                        <li><a href="">Melhores Criticas</a></li>
+                        <li><a href="">Melhores Autores</a></li>
+                        <li><a href="">Criticas</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3 col-lg-1 ">
+                    <ul >
+                        <li><a href="">Cosplay</a></li>
+                        <li><a href="">Animes</a></li>
+                        <li><a href="">Game</a></li>
+                        <li><a href="">Noticias</a></li>
+                        <li><a href="">Eventos</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3  col-lg-1 ">
+                    <ul >
+                        <li><a href="">Series & TV</a></li>
+                        <li><a href="">Noticias</a></li>
+                        <li><a href="">Lançamentos</a></li>
+                        <li><a href="">Autores</a></li>
+                        <li><a href="">Resenhas</a></li>
+                    </ul>
+                </div>
+                <div class="col-3 col-lg-1">
+</div>
+                <div class="col-lg-7">
+                    <p class="text-center">Participantes</p>
+                   <div class="row">
+                        <div class="col-3 col-lg-3">
+                            <figure>
+                                <img src="/projeto-integrador-II/perfil.png" class="img-fluid" >
+                                <figcaption>Gilberto</figcaption>
+                            </figure>
+                        </div>
+                        <div class="col-3 col-lg-3 ">
+                        <figure>
+                                <img src="/projeto-integrador-II/perfil.png" class="img-fluid">
+                                <figcaption>José Guilherme</figcaption>
+                            </figure>
+                        </div>
+                        <div class="col-3 col-lg-3">
+                        <figure>
+                                <img src="/projeto-integrador-II/perfil.png" class="img-fluid">
+                                <figcaption>Melissa</figcaption>
+                            </figure>
+                        </div>
+                        <div class="col-3 col-lg-3">
+                            <figure>
+                                <img src="/projeto-integrador-II/perfil.png" class="img-fluid">
+                                <figcaption>Yasmin</figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <p class="text-center">
+                            "Arte é a cultura popular Existe em qualquer parte, está em todo lugar."
+                        </p>
+                        <p class="text-end">
+                         Real Poeta(Tiago Suil)
+                        </p>
+                    </div>
+                </div>
+        </div>
+    
