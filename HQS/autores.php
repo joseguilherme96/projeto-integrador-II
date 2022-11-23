6d47443961e9e2f953f93f96e6588b140694ae7f
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2022-11-23 00:01:35 -0300

    Update and rename autores.html to autores.php

--- HQS/autores.php
+++ HQS/autores.php
@@ -7,17 +7,19 @@
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   </head>
   <body>
-    
+    <header>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    
-
-<nav>
+    </header>
 
 
-</nav>
 
 
  <div class="container text-center">
+   <nav>
+
+
+  <?php include "../nav.php"?>
+</nav>
   <div class="row">
    
  <section>
@@ -125,7 +127,7 @@
  </div>
 
 <footer>
-
+  <?php include "../rodape.php"?>
 </footer>
 </body>
 
