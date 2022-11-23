3bc4abc3fdd67105d90f05676c2525f30c841ecb
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2022-11-23 00:00:18 -0300

    Update and rename critica.html to critica.php

--- HQS/critica.php
+++ HQS/critica.php
@@ -8,17 +8,19 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   </head>
 <body>
+  
+
+    <header>
     <h1>CRITICA</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 
-
-
-    <header>
-    <nav>
-   
-      </nav>
       </header>
-    
+  
+  <div class="container">
+    <nav>
+       <?php include "../nav.php"?>
+    </nav>
+ 
 <div class="container-sm">
 
 <article>
@@ -62,10 +64,10 @@
      </div>
 
 </article>
-
+</div>
 </div>
       <footer>
-        
+         <?php include "../rodape.php"?>
       </footer>
 </body>
 </html>
