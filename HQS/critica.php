e1d924978a4b51b6744aa467c6dedca0f68b3333
joseguilherme96 <87539880+joseguilherme96@users.noreply.github.com>
2022-11-23 22:03:54 -0300

    Ajustado Menu

--- HQS/critica.php
+++ HQS/critica.php
@@ -5,24 +5,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>HQS</title>
+    <link href="../css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="../node_modules/bootstrap-icons/font/bootstrap-icons.css">
   </head>
 <body>
-  
-
-    <header>
-    <h1>CRITICA</h1>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
-      </header>
-  
+  <?php include "../nav.php"?>
   <div class="container">
-    <nav>
-       <?php include "../nav.php"?>
-    </nav>
- 
-<div class="container-sm">
-
+    <div class="container-sm">
 <article>
   <div class="col-12 col-sm-12 col-lg-12 p-1"><!--col <= 576px; col-sm >=576px col-lg >=992px -->
 <div class="card mb-3">
