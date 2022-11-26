cdf160610a1b4af3f29d4df128ddaafab4526ac5
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2022-11-26 15:01:51 -0300

    codigo ajustado

--- HQS/topfive.php
+++ HQS/topfive.php
@@ -1,157 +1,62 @@
 <!doctype html>
 <html lang="en">
+  
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>HQS</title>
+    <title>Topfive</title>
+    <link href="../css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="../node_modules/bootstrap-icons/font/bootstrap-icons.css">
   </head>
 <body>
-     <header>
+  <body>
 
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
-
-
-</header>
-
-
-
-
-
- <div class="container text-center">
-
-<nav>
-<?php include "../nav.php"?>
+  <div class="container">
+    <nav>
+    <?php include "../nav.php"?>
 </nav>
+    <section class="row pt-5 "> 
 
-          
-<section>      
-<h2> seção</h2>          
-<div class="row">
-  <div class="col-12 col-sm-6 col-lg-3 p-1">
-    <div class="text-center">
-      <figure>
-        <img src="https://via.placeholder.com/250x350">
-        <figcaption>Maze Runner</figcaption>
-      </figure>
-    </div>
-    <div class="text-left">
-      <h5> Lorem ipsum dolor sit amet, consectetur </h5>
-      <h6>
-        Por <a href="">Yasmin</a>  28/10/20222
-      </h6>
-      <p>
-      Lorem ipsum dolor sit amet, consecteturLorem ipsum
-      dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
-      consecteturLorem ipsum dolor sit amet,
-      </p>
+      <div class="col-12 col-sm-6 col-lg-12 text-center pt-5 "> 
+      <img src="https://via.placeholder.com/200x200 " alt="" >
+      </div>  
 
-    </div>
+      <div class="col-12 col-sm-6 col-lg-12 pt-5 pb-5 text-start "> 
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, placeat? Quis minima alias natus praesentium, eos perspiciatis, quisquam numquam, ab reiciendis explicabo eius! Exercitationem ex nesciunt officia quo provident vero! 
+        </p>
+      </div>
+   
+      <div class="col-12 col-sm-6 col-lg-12 pt-5 text-center"> <img src="https://via.placeholder.com/200x200" alt="">
+      </div>  
 
-    </div>
-    </div>
-</section>
+      <div class="col-12 col-sm-6 col-lg-12  pt-5 pb-5 text-start " > 
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia error tempore, consequatur aliquid sit similique. Corporis deleniti, est quae saepe voluptates, quibusdam esse facere ducimus voluptas modi, hic incidunt maxime? </p>
+      </div>
 
-<section>
-<h2> seção</h2>  
-    <div class="text-right">
-      <a href="">Ler Mais</a>
+    </section>
+     
+    <section class="row pt-5 "> 
+
+      <div class="col-12 col-sm-6 col-lg-12 pt-5 text-center" > <img src="https://via.placeholder.com/200x200" alt="">
+      </div>
+
+      <div class="col-12 col-sm-6 col-lg-12 pt-5 pb-5 text-start" > 
+        <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error quis quisquam hic praesentium consectetur iusto excepturi rerum voluptatibus neque repellat deserunt quae necessitatibus, labore explicabo est ex molestiae illum asperiores!</p> 
+      </div>
+
+
+      <div class="col-12 col-sm-6 col-lg-12 pt-5 text-center" > <img src="https://via.placeholder.com/200x200" alt="">
+      </div>
+    
+      <div class="col-12 col-sm-6 col-lg-12 pt-5 pb-5 text-start " > 
+        <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates consequatur nemo eveniet quod molestiae esse culpa maiores facere dolorem itaque iure dignissimos obcaecati amet, voluptate facilis laboriosam. Repudiandae, ipsam exercitationem? </p>
+      </div>
+
+    </section>
 
   </div>
-  <div class="col-12 col-sm-6 col-lg-3 p-1">
-    <div class="text-center">
-      <figure>
-        <img src="https://via.placeholder.com/250x350">
-        <figcaption>Maze Runner</figcaption>
-      </figure>
-    </div>
-</div>
-    <div class="text-left">
-      <h5>
-        Lorem ipsum dolor sit amet, consectetur 
-      </h5>
-      <h6>
-        Por <a href="">Yasmin</a>  28/10/20222
-      </h6>
-      <p>
-      Lorem ipsum dolor sit amet, consecteturLorem ipsum
-      dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
-      consecteturLorem ipsum dolor sit amet,
-      </p>
-    </div>
+<footer><?php include "../rodape.php"?></footer>
   
-</section>
-
-<section>
-<h2> seção</h2>
-      <div class="text-right">
-        <a href="">Ler Mais</a>
-      </div>
-
-    <div class="col-12 col-sm-6 col-lg-3 p-1">
-    <div class="text-center">
-    <figure><img src="https://via.placeholder.com/250x350">
-    <figcaption>Maze Runner</figcaption>
-    </figure>
-      </div>
-</div>
-</section>
-<section>
-      <div class="text-left">
-      <h5>
-        Lorem ipsum dolor sit amet, consectetur 
-      </h5>
-      <h6>
-        Por <a href="">Yasmin</a>  28/10/20222
-      </h6>
-      <p>
-      Lorem ipsum dolor sit amet, consecteturLorem ipsum
-      dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
-      consecteturLorem ipsum dolor sit amet,
-      </p>
-</div>
-</section>
-<section>
-<h2>seção</h2>
-      <div class="text-right">
-        <a href="">Ler Mais</a>
-      </div>
-  
-  <div class="col-12 col-sm-6 col-lg-3 p-1">
-    <div class="text-center">
-      <figure>
-        <img src="https://via.placeholder.com/250x350">
-        <figcaption>Maze Runner</figcaption>
-      </figure>
-    </div>
- </div>
-
-</section>
-<section>
-<h2>seção</h2>
-    <div class="text-left">
-      <h5>
-        Lorem ipsum dolor sit amet, consectetur 
-      </h5>
-      <h6>
-        Por <a href="">Yasmin</a>  28/10/20222
-      </h6>
-       <p>
-      Lorem ipsum dolor sit amet, consecteturLorem ipsum
-      dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
-      consecteturLorem ipsum dolor sit amet,
-   </p>
-        <div class="text-right">
-        <a href="">Ler Mais</a>
-   </div>
-   </div>
-   </section>
-
-  </div>
-<footer>
-<?php include "../rodape.php"?>
-</footer>
 </body>
 </html>
