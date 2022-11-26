ffb11b0286b8420abe736301fd0fd980ab61a29e
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2022-11-26 14:17:38 -0300

    codigo ajustado

--- HQS/autores.php
+++ HQS/autores.php
@@ -13,126 +13,57 @@
 
 
 
+<?php include "../nav.php"?>
 
- <div class="container text-center">
-   <nav>
-
-
-  <?php include "../nav.php"?>
-</nav>
-  <div class="row">
+<div class="container">
    
- <section>
-    <div class="col-12 col-sm-12 col-lg-12 p-1"><!--col <= 576px; col-sm >=576px col-lg >=992px -->
-      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="https://via.placeholder.com/1200x350" class="img-fluid" alt= "">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>First slide label</h5>
-              <p>Some representative placeholder content for the first slide.</p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="https://via.placeholder.com/1200x350" class="img-fluid" alt= "">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Second slide label</h5>
-              <p>Some representative placeholder content for the second slide.</p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src="https://via.placeholder.com/1200x350" class="img-fluid" alt = ''>
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Third slide label</h5>
-              <p>Some representative placeholder content for the third slide.</p>
-            </div>
-          </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
+  
+    <section class="row pt-5 "> 
+
+      <div class="col-6 col-sm-12 col-lg-6 text-center pt-5 "> 
+      <img src="https://via.placeholder.com/200x200 " alt="" >
+      </div>  
+
+      <div class="col-6 col-sm-12 col-lg-6 pt-5 pb-5 text-start "> 
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, placeat? Quis minima alias natus praesentium, eos perspiciatis, quisquam numquam, ab reiciendis explicabo eius! Exercitationem ex nesciunt officia quo provident vero! 
+        </p>
+      </div>
+   
+      <div class="col-6 col-sm-12 col-lg-6 pt-5 text-center"> <img src="https://via.placeholder.com/200x200" alt="">
+      </div>  
+
+      <div class="col-6 col-sm-12 col-lg-6  pt-5 pb-5 text-start " > 
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia error tempore, consequatur aliquid sit similique. Corporis deleniti, est quae saepe voluptates, quibusdam esse facere ducimus voluptas modi, hic incidunt maxime? </p>
+      </div>
+
+    </section>
+     
+    <section class="row pt-5 "> 
+
+      <div class="col-6 col-sm-12 col-lg-6 pt-5 text-center" > <img src="https://via.placeholder.com/200x200" alt="">
+      </div>
+
+      <div class="col-6 col-sm-12 col-lg-6 pt-5 pb-5 text-start" > 
+        <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error quis quisquam hic praesentium consectetur iusto excepturi rerum voluptatibus neque repellat deserunt quae necessitatibus, labore explicabo est ex molestiae illum asperiores!</p> 
       </div>
 
 
-</div>
+      <div class="col-6 col-sm-12 col-lg-6 pt-5 text-center" > <img src="https://via.placeholder.com/200x200" alt="">
+      </div>
+    
+      <div class="col-6 col-sm-12 col-lg-6 pt-5 pb-5 text-start " > 
+        <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates consequatur nemo eveniet quod molestiae esse culpa maiores facere dolorem itaque iure dignissimos obcaecati amet, voluptate facilis laboriosam. Repudiandae, ipsam exercitationem? </p>
+      </div>
 
-</section>
-      <aside>
-        <div class="row">
-         <div class="col-12 col-sm-12 col-lg-12 p-1"><!--col <= 576px; col-sm >=576px col-lg >=992px -->
-     
-        <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h6>
-           </div>
-</div>
-
-       
-</aside>
-
-  <div class="row">
-    <div class="col-12 col-sm-12 col-lg-12 p-1"><!--col <= 576px; col-sm >=576px col-lg >=992px -->
-    <img src="https://via.placeholder.com/900x350" class="img-fluid" alt = "">
-    <div>Super</div>
-    </div>
-</div>
-
-
-
-   <div class="row">
-    <div class="col-12 col-sm-12 col-lg-12 p-1"><!--col <= 576px; col-sm >=576px col-lg >=992px -->
-
-   <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h6>
-  
- </div>
-</div>
+    </section>
 
   
 
-
-  <div class="row">
-    <div class="col-12 col-sm-12 col-lg-12 p-1"><!--col <= 576px; col-sm >=576px col-lg >=992px -->
-    <img src="https://via.placeholder.com/900x350" class="img-fluid" alt = " ">
-    <div>Super</div>
-    </div>
-</div>
-
-
-  
-  <aside>
-  
-   <div class="row">
-    <div class="col-12 col-sm-12 col-lg-12  p-1"><!--col <= 576px; col-sm >=576px col-lg >=992px -->
-
-   <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h6>
-</div>
-</div>
-  
-  </aside>
-  
-
- </div>
-
- </div>
-
-<footer>
+    </section>
+  </div>
   <?php include "../rodape.php"?>
-</footer>
 </body>
-
 </html>
-
 
      
  
