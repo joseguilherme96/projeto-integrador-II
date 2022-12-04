dfff8862bddc10e85092a7aa937392326b92a010
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2022-12-04 19:45:27 -0300

    now

--- quemsomos.php
+++ quemsomos.php
@@ -15,7 +15,7 @@
     <section class="row pt-5 "> 
 
       <div class="col-12 col-sm-6 col-lg-6 text-center pt-5 "> 
-      <img src="/projeto-integrador-II/cinema/jose.jpg" alt="Aqui temos a imagem do José" >
+      <img src="/projeto-integrador-II/jose.jpg" alt="Aqui temos a imagem do José" >
       </div>  
 
       <div class="col-12 col-sm-6 col-lg-6 pt-5 pb-5 text-start "> 
@@ -23,7 +23,7 @@
           <ul><li>RA: 2650832213006 <li>
           <li>Biografia: Integrante do grupo Pirulito Malignos de açucar e mel, reside em Mairinque, tem 25 anos e está cursando o segundo Semestre de Sistemas para internet na FATEC de São Roque. <li>
           <li>Função no Grupo: Gerente<li>
-          <li>Responsável: Página Series e TV <li>
+          <li>Responsável: Cinema <li>
           <li><a href="/projeto-integrador-II/cinema/index.php">Cinema</a> <li>
           <li><a href="/projeto-integrador-II/cinema/entrevista.php">Entrevistas</a> <li>
           <li><a href="/projeto-integrador-II/cinema/filmes.php">Filmes</a> <li>
