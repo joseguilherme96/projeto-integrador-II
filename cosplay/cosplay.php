75e393ee3b0f874488ba605ca35eb59845e631cb
GILBERTO <106849333+Giliarde0@users.noreply.github.com>
2022-12-05 20:39:41 -0300

    Site completo

--- cosplay/cosplay.php
+++ cosplay/cosplay.php
@@ -11,12 +11,14 @@
 <?php include "../nav.php"?>
   <div class="container">
    
-  
+    <div class="col-12 text-center pt-5">
+    <h1 class="cor-letra"> COSPLAY </h1>
+    </div>
     <section class="row pt-5 "> 
-
+      
       <div class="col-12 col-sm-6 col-lg-3 text-center pt-5 "> 
-      <img src="/projeto-integrador-II/imgcosplay//kodia.jpg " alt="Ahsoka Tano - Star Wars
-Incrivel Cosplay do personagem Ahsoka Tano feita por uma fã da grande série Star Wars surpreendeu nas redes sociais, a fã descreve *Esse cosplay foi um verdadeiro desafio pra mim, principalmente a maquiagem, e aprendi muito fazendo.Mal posso esperar para usá-la na Star Wars Celebration em Londres no ano que vem." >
+      <a href="imagem - https://www.instagram.com/p/ClEcb9ojfda/?utm_source=ig_embed&ig_rid=ec94feb5-a4f1-49f9-bc25-1625c4ae9ef5"><img src="/projeto-integrador-II/imgcosplay//kodia.jpg " alt="Ahsoka Tano - Star Wars
+Incrivel Cosplay do personagem Ahsoka Tano feita por uma fã da grande série Star Wars surpreendeu nas redes sociais, a fã descreve *Esse cosplay foi um verdadeiro desafio pra mim, principalmente a maquiagem, e aprendi muito fazendo.Mal posso esperar para usá-la na Star Wars Celebration em Londres no ano que vem." > </a>
       </div>  
 
       <div class="col-12 col-sm-6 col-lg-3 pt-5 pb-5 text-start "> 
@@ -24,8 +26,8 @@ Incrivel Cosplay do personagem Ahsoka Tano feita por uma fã da grande série St
         </p>
       </div>
    
-      <div class="col-12 col-sm-6 col-lg-3 pt-5 text-center"> <img src="/projeto-integrador-II/imgcosplay//arlequina.jpg" alt="Deadpool e Arlequina
-Deadpool e Arlequina juntos em incrivel cosplay feito por fã, quem conhece sabe que os dois personagem são de casas de super-herois diferentes, então essa amizade é bem improvavel acontecer um dia, em algum filme, mas os fãs quebram barreiras. ">
+      <div class="col-12 col-sm-6 col-lg-3 pt-5 text-center"><a href="https://www.instagram.com/jennifercosplays/?utm_source=ig_embed&ig_rid=fc50bd0c-6612-4040-a0c9-3b7372100f2c"> <img src="/projeto-integrador-II/imgcosplay//arlequina.jpg" alt="Deadpool e Arlequina
+Deadpool e Arlequina juntos em incrivel cosplay feito por fã, quem conhece sabe que os dois personagem são de casas de super-herois diferentes, então essa amizade é bem improvavel acontecer um dia, em algum filme, mas os fãs quebram barreiras. "> </a>
       </div>  
 
       <div class="col-12 col-sm-6 col-lg-3  pt-5 pb-5 text-start " > 
@@ -36,8 +38,8 @@ Deadpool e Arlequina juntos em incrivel cosplay feito por fã, quem conhece sabe
      
     <section class="row pt-5 "> 
 
-      <div class="col-12 col-sm-6 col-lg-3 pt-5 text-center" > <img src="/projeto-integrador-II/imgcosplay//gowcosplay.jpg" alt="God of War Ragnarök
-Cosplay surpreende fãs da grande franquia, com o recém lançamendo do jogo God of War Ragnarök, que está sendo um sucesso absurdo, aclamada em todo o mundo, alguns comentarios positivos surgiram como este *A equipe envolvida na construção desses cosplays é simplesmente incrível*">
+      <div class="col-12 col-sm-6 col-lg-3 pt-5 text-center" > <a href="https://animenew.com.br/god-of-war-ragnarok-cosplay-impecavel-deixa-fas-surpresos/"><img src="/projeto-integrador-II/imgcosplay//gowcosplay.jpg" alt="God of War Ragnarök
+Cosplay surpreende fãs da grande franquia, com o recém lançamendo do jogo God of War Ragnarök, que está sendo um sucesso absurdo, aclamada em todo o mundo, alguns comentarios positivos surgiram como este *A equipe envolvida na construção desses cosplays é simplesmente incrível*"> </a>
       </div>
 
       <div class="col-12 col-sm-6 col-lg-3 pt-5 pb-5 text-start" > 
@@ -45,8 +47,8 @@ Cosplay surpreende fãs da grande franquia, com o recém lançamendo do jogo God
       </div>
 
 
-      <div class="col-12 col-sm-6 col-lg-3 pt-5 text-center" > <img src="/projeto-integrador-II/imgcosplay//capitaoamerica.jpeg" alt="Capitão America
-Incrivel cosplay de capitão america surpreende na CCXP, diretamente de guerra infinita, um dos herois mais queridos do mundo dos cinemas.">
+      <div class="col-12 col-sm-6 col-lg-3 pt-5 text-center" > <a href="https://twitter.com/omelete/status/1144281826183327744"><img src="/projeto-integrador-II/imgcosplay//capitaoamerica.jpeg" alt="Capitão America
+Incrivel cosplay de capitão america surpreende na CCXP, diretamente de guerra infinita, um dos herois mais queridos do mundo dos cinemas."> </a>
       </div>
     
       <div class="col-12 col-sm-6 col-lg-3 pt-5 pb-5 text-start " > 
@@ -58,16 +60,16 @@ Incrivel cosplay de capitão america surpreende na CCXP, diretamente de guerra i
     <section class="row pt-5 ">
 
       <div class="col-12 col-sm-6 col-lg-3 pt-5 text-center" > 
-        <img src="/projeto-integrador-II/imgcosplay//gueen.jpg" alt="Gueen
-Fã faz cosplay de Gueen, uma das personagens mais amada da serie de filmes do Homem Aranha, além desses a fã ja fez diversos outros personagens.">
+      <a href="https://www.instagram.com/jennifercosplays/?utm_source=ig_embed&ig_rid=fc50bd0c-6612-4040-a0c9-3b7372100f2c"><img src="/projeto-integrador-II/imgcosplay//gueen.jpg" alt="Gueen
+Fã faz cosplay de Gueen, uma das personagens mais amada da serie de filmes do Homem Aranha, além desses a fã ja fez diversos outros personagens."> </a>
       </div>
     
       <div class="col-12 col-sm-6 col-lg-3 pt-5 pb-5 text-start" > 
         <p> <h6 class="cor-letra">Gueen</h6> Fã faz cosplay de Gueen, uma das personagens mais amada da serie de filmes do Homem Aranha, além desses a fã ja fez diversos outros personagens.</p>
       </div>
 
-      <div class="col-12 col-sm-6 col-lg-3 pt-5 pb-5 text-center" > <img src="/projeto-integrador-II/imgcosplay//sylvie.jpg" alt="Sylvie
-Quem ai conhece a grande *vilã* da serie Loki? Sylvie, uma personagem bastante imponente e que faz parte de um dos grandes estopims da marvel.">
+      <div class="col-12 col-sm-6 col-lg-3 pt-5 pb-5 text-center" > <a href="https://www.instagram.com/jennifercosplays/?utm_source=ig_embed&ig_rid=fc50bd0c-6612-4040-a0c9-3b7372100f2c"> <img src="/projeto-integrador-II/imgcosplay//sylvie.jpg" alt="Sylvie
+Quem ai conhece a grande *vilã* da serie Loki? Sylvie, uma personagem bastante imponente e que faz parte de um dos grandes estopims da marvel."></a>
       </div>
         
       <div class="col-12 col-sm-6 col-lg-3 pt-5 pb-5 text-start"> 
--- cosplay/eventos.php
+++ cosplay/eventos.php
@@ -5,17 +5,20 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="/projeto-integrador-II/css//tema.css" rel="stylesheet" >
 </head>
 <body>
 <?php include "../nav.php"?>
     <div class="container">
-  
+    <div class="col-12 text-center pt-5">
+    <h1 class="cor-letra "> EVENTOS </h1>
+    </div>
     <div class="row text-center">
         <div class="col-12 col-sm-12 col-lg-12 text-center  pb-5 pt-5"> 
 
           <picture>
-            <source srcset="https://via.placeholder.com/960x200" class="img-fluid" media="(min-width:960px)">
-           <img src="https://via.placeholder.com/400x200" alt="Esta é uma imagem vazia" class="img-fluid">
+            <source srcset="/projeto-integrador-II/imgcosplay//eventos.png" class="img-fluid" media="(min-width:960px)">
+           <img src="/projeto-integrador-II/imgcosplay//eventos2.png" alt="Esta é uma imagem vazia" class="img-fluid">
           </picture>
     
         </div>
@@ -23,39 +26,30 @@
     
       <div class="row text-center pb-5 pt-5">
 
-        <div class="col-6 col-sm-6 col-lg-6"> <img src="https://via.placeholder.com/120x120" alt="">
+        <div class="col-6 col-sm-6 col-lg-6"> <h6 class="cor-letra" > E3 </h6 > <a href="https://e3expo.com"><img src="/projeto-integrador-II/imgcosplay//E3evento.png" alt=""></a>
+        <p>A Electronic Entertainment Expo, mais conhecida como E3, é uma feira internacional dedicada a jogos eletrônicos. É considerada a mais importante do gênero, por reunir novidades relativas a lançamentos e tendências de mercado de várias empresas do setor dos jogos.</p>
         
         </div>
 
-        <div class="col-6 col-sm-6 col-lg-6" > <img src="https://via.placeholder.com/120x120" alt="">
+        <div class="col-6 col-sm-6 col-lg-6" > <h6 class="cor-letra"> CCXP </h6> <a href="https://www.ccxp.com.br"> <img src="/projeto-integrador-II/imgcosplay//ccxp.png" alt=""> </a>
+        <p>CCXP é um evento brasileiro de cultura pop nos moldes da San Diego Comic-Con cobrindo as principais áreas dessa indústria como: vídeo-games, histórias em quadrinhos, filmes e séries para TV. </p>
 
         </div>
       </div>
 
       <div class="row text-center">
         
-        <div class="col-6 col-sm-6 col-lg-6" > <img src="https://via.placeholder.com/120x120" alt=""> 
-        
+        <div class="col-6 col-sm-6 col-lg-6 pb-5 pt-5" > <h6 class="cor-letra" > Summer Game Fast </h6 > <a href="https://www.summergamefest.com"> <img src="/projeto-integrador-II/imgcosplay//summergamerfast.jpg" alt=""> </a>
+        <p>Summer Game Fest é mais um dos grandes eventos recheados de anúncios e novidades do mundo gamer. Com o cancelamento da E3, o showcase se tornou um dos mais esperados do ano no mercado de jogos. </p>
         </div>
         
-        <div class="col-6 col-sm-6 col-lg-6" > <img src="https://via.placeholder.com/120x120" alt="">
+        <div class="col-6 col-sm-6 col-lg-6 pt-5 pb-5 " > <h6 class="cor-letra" > Game XP </h6 > <a href=" https://www.gamexp.com.br" > <img src="/projeto-integrador-II/imgcosplay//gamexp.png" alt=""> </a>
+        <p> Maior evento gamer da América Latina, a Game XP 2021 é realizada neste fim de semana, pela primeira vez de forma virtual para o público. O festival contará com duas disputas de esports: a Taça Game XP de Free Fire no sábado e a Grrrls League</p>
           
         </div>
       
-        <div class="col-4 col-sm-4 col-lg-4 pb-5 pt-5" > <img src="https://via.placeholder.com/120x120" alt="">  
-
-        </div>
-        
-       <div class="col-4 col-sm-4 col-lg-4 pb-5 pt-5" > <img src="https://via.placeholder.com/120x120" alt="">
-          
-        </div>
-        
-        <div class="col-4 col-sm-4 col-lg-4 pb-5 pt-5" > <img src="https://via.placeholder.com/120x120" alt="">  
-      </div>
-    
-        </div> 
     </div>
-    
+
 
   <?php include "../rodape.php"?>
 </body>
