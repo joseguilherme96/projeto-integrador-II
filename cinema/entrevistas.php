b2825448b5ff69f3a82b0d00652158631134694a
José Guilherme <jose.guilherme96@outlook.com.br>
2022-12-05 12:33:34 -0300

    Finalizado pagina literatura

--- cinema/entrevistas.php
+++ cinema/entrevistas.php
@@ -9,65 +9,80 @@
     <link href="../css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="../node_modules/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="../css/tema.css">
 </head>
 <body>
+    
+<?php include '../nav.php'; ?>
     <div class="container">
-        <?php include '../nav.php'; ?>
         <div class="row">
-            <div class="col-12">
-                <a href="../index.php">Home</a> / <a href="index.php">Cinema</a> / <a href="literatura.php">Entrevistas</a> 
+            <div class="col-12 cor-letra">
+                <a href="../index.php" class="cor-letra">Home</a> / <a href="index.php" class="cor-letra">Cinema</a> / <a href="literatura.php" class="cor-letra">Entrevistas</a> 
             </div>
         </div>
-        <div class="row justify-content-center">
-            <div class="col-5 ">
-                <input type="text" class="form-control " placeholder="Pesquise sobre filmes,noticias..">
-            </div>
-            <div class="col-2 p-2">
-                <i class="bi-search "></i>
-            </div>
-        </div>
+        <?php include "../barra-de-pesquisa.php";?>
         <div class="row">
             <div class="col-12 text-center">
-                <h1>ENTREVISTAS COM ATORES</h1>
+                <h1 class="cor-letra">ENTREVISTAS COM ATORES</h1>
             </div> 
         </div>
         <div class="row">
             <section class="col-12 col-sm-6 col-lg-4 p-1">
-                <h2 class="cor-letra">Jason Stham - Velozes & Furiosos 8</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus rem eveniet quas! Nemo eveniet sint voluptatum fugit eius, incidunt voluptatem illum quae distinctio earum neque iste deserunt quaerat consectetur libero?</p>
-                <img src="https://via.placeholder.com/294x195" class="img-fluid" alt="Foto ator">
-                <a href="entrevista-com-autores.php" class="d-block text-end">Ler Mais</a>   
+            <div class="row">
+        <h2 class="cor-letra">Jason Stham</h2>
+        </div>
+        <div class="row">
+          <img src="img/jason-statham.jpg"  class="img-fluid" alt="Foto Autor">
+        </div>
+        <div class="row">
+          <p>Conhecido como o cara durão de Carga Explosiva, Os Mercenários e Velozes & Furiosos 7, Jason Statham fez poucas comédias em sua carreira. </p>
+        </div>
+        <div class="row text-end">
+        <a href="entrevista-com-autores.php">Ler Mais</a>
+        </div>   
             </section>
             <section class="col-12 col-sm-6 col-lg-4 p-1">
-                <h2>Ator 1</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus rem eveniet quas! Nemo eveniet sint voluptatum fugit eius, incidunt voluptatem illum quae distinctio earum neque iste deserunt quaerat consectetur libero?</p>
-                <img src="https://via.placeholder.com/294x195" class="img-fluid" alt="Foto ator">
-                <a href="" class="d-block text-end">Ler Mais</a>
+            <div class="row">
+          <h2 class="cor-letra">Vin Diesel</h2>
+        </div>
+        <div class="row">
+          <img src="img/vin-diesel.jpg" class="img-fluid" alt="Foto Autor">
+        </div>
+        <div class="row">
+          <p>Vin diesel fala sobre o filme Reativado que estreiou no dia 19 de janeiro.</p>
+        </div>
+        <div class="row text-end">
+        <a href="entrevista-com-autores.php">Ler Mais</a>
+        </div>
             </section>
             <section class="col-12 col-sm-6 col-lg-4 p-1">
-                <h2>Ator 1</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus rem eveniet quas! Nemo eveniet sint voluptatum fugit eius, incidunt voluptatem illum quae distinctio earum neque iste deserunt quaerat consectetur libero?</p>
-                <img src="https://via.placeholder.com/294x195" class="img-fluid" alt="Foto ator">
-                <a href="" class="d-block text-end">Ler Mais</a>
+            <div class="row">
+          <h2 class="cor-letra">Camille Rowe</h2>
+        </div>
+        <div class="row">
+          <img src="img/camille-rowe.jpg" class="img-fluid" alt="Foto Autor">
+        </div>
+        <div class="row">
+          <p>Camille Rowe a protagonista do primeiro fashion film Journeys by Mango a apresentar a coleção feminina mais em</p>
+        </div>
+        <div class="row text-end">
+        <a href="entrevista-com-autores.php">Ler Mais</a>
+        </div>
             </section>
-            <section class="col-12 col-sm-6 col-lg-4 p-1">
-                <h2>Ator 1</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus rem eveniet quas! Nemo eveniet sint voluptatum fugit eius, incidunt voluptatem illum quae distinctio earum neque iste deserunt quaerat consectetur libero?</p>
-                <img src="https://via.placeholder.com/294x195" class="img-fluid" alt="Foto ator">
-                <a href="" class="d-block text-end">Ler Mais</a>
-            </section>
-            <section class="col-12 col-sm-6 col-lg-4 p-1">
-                <h2>Ator 1</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus rem eveniet quas! Nemo eveniet sint voluptatum fugit eius, incidunt voluptatem illum quae distinctio earum neque iste deserunt quaerat consectetur libero?</p>
-                <img src="https://via.placeholder.com/294x195" class="img-fluid" alt="Foto ator">
-                <a href="" class="d-block text-end">Ler Mais</a>
-            </section>
-            <section class="col-12 col-sm-6 col-lg-4 p-1">
-                <h2>Ator 1</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus rem eveniet quas! Nemo eveniet sint voluptatum fugit eius, incidunt voluptatem illum quae distinctio earum neque iste deserunt quaerat consectetur libero?</p>
-                <img src="https://via.placeholder.com/294x195" class="img-fluid" alt="Foto ator">
-                <a href="" class="d-block text-end">Ler Mais</a>
-            </section>
+            <section class="col-12 col-sm-12 col-lg-12 p-1">
+            <div class="row">
+          <h2 class="cor-letra">Audrey Tautou</h2>
+        </div>
+        <div class="row">
+          <img src="img/audrey-tautou.jpg" class="img-fluid"alt="Foto Autor">
+        </div>
+        <div class="row">
+          <p>Audrey Justine Tautou é uma atriz francesa. Reconhecida na França por sua atuação em Vénus beauté que lhe rendeu o prêmio César de Atriz Revelação</p>
+        </div>
+        <div class="row text-end">
+        <a href="entrevista-com-autores.php">Ler Mais</a>
+        </div>
+            </section>  
         </div>
     </div>
     <?php include "../rodape.php"; ?>
