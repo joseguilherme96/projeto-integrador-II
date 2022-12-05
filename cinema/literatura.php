55b6f0435cdb500486759c6851e322d83e19e78c
José Guilherme <jose.guilherme96@outlook.com.br>
2022-12-05 12:15:03 -0300

    Pagina literatura.php finalizada

--- cinema/literatura.php
+++ cinema/literatura.php
@@ -8,178 +8,131 @@
     <link href="../css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="../node_modules/bootstrap-icons/font/bootstrap-icons.css">
-</head>
+    <link rel="stylesheet" href="../css/tema.css">
+  </head>
 <body>
+<?php include '../nav.php'; ?>
     <div class="container">
-        <?php include '../nav.php'; ?>
         <div class="row">
-            <div class="col-12">
-                <a href="../index.php">Home</a> / <a href="index.php">Cinema</a> / <a href="literatura.php">Literatura</a> 
+            <div class="col-12 cor-letra" >
+                <a href="../index.php" class="cor-letra">Home</a> / <a href="index.php" class="cor-letra">Cinema</a> / <a href="literatura.php" class="cor-letra">Literatura</a> 
             </div>
         </div>
-        <div class="row justify-content-center">
-            <div class="col-5 ">
-                <input type="text" class="form-control " placeholder="Pesquise sobre filmes,noticias..">
-            </div>
-            <div class="col-2 p-2">
-                <i class="bi-search "></i>
-            </div>
-        </div>
+        <?php include "../barra-de-pesquisa.php";?>
         <div class="row">
             <div class="col-12 text-center">
-                <h1>FILMES INSPIRADO EM LIVROS</h1>
+                <h2 class="d-inline cor-letra text-start">FILMES INSPIRADO EM LIVROS</h2>
             </div> 
         </div>
         <div class="row">
         <section class="col-12 col-sm-6 col-lg-4 p-1">
           <div class="text-center">
-            <picture>
-              <source srcset="https://via.placeholder.com/113x154" media="(min-width:992px)">
-              <img src="https://via.placeholder.com/250x250" alt="Capa Livro">
-            </picture>
-            <h2>Jogador Nº 1</h2>
+            <figure>
+              <img src="img/jogador-n1.jpg" alt="Capa Livro">
+              <figcaption>Jogador Nº 1</figcaption>
+            </figure>
           </div>
           <div class="text-left">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h5>Lorem ipsum dolor sit amet, consectetur </h5>
-                    <h6>Por <a href="">Yasmin</a>  28/10/2022</h6>
-                </div>
-            </div>
+            <h4 class="cor-letra">
+            Jogador Nº 1 | Crítica
+           
+            </h4>
+            <h5 class="cor-letra-D2D2D2">
+            A volta do Steven Spielberg moleque
+            </h5>
+            <h6>
+              Por <a href="../autores.php">ÉRICO BORGO</a>  19/03/2018
+              <!--https://www.omelete.com.br/filmes/criticas/jogador-no-1-critica-->
+            </h6>
             <p>
-            Lorem ipsum dolor sit amet, consecteturLorem ipsum
-            dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
-            consecteturLorem ipsum dolor sit amet,
+            Ninguém melhor que Steven Spielberg para levar às telas a adaptação de Jogador Nº 1 de Ernest Cline.
+             O cineasta, afinal, é a maior influência (o maior responsável, na verdade) 
+             por tudo aquilo que inspirou o autor.
             </p>
           </div>
           <div class="text-end">
-            <a href="artigo-literatura.php?noticia=2">Ler Mais</a>
+            <a href="artigo-literatura.php?cod=1">Ler Mais</a>
           </div>
         </section>
         <section class="col-12 col-sm-6 col-lg-4 p-1">
-          <div class="text-center">
-            <picture>
-              <source srcset="https://via.placeholder.com/113x154" media="(min-width:992px)">
-              <img src="https://via.placeholder.com/250x250"  alt="Capa Livro">
-            </picture>
-            <h2>Harry Potter</h2>
+        <div class="text-center">
+            <figure>
+              <img src="img/jogos-vorazes.jpg" alt="Capa Livro">
+              <figcaption>Jogos Vorazes</figcaption>
+            </figure>
           </div>
           <div class="text-left">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h5>Lorem ipsum dolor sit amet, consectetur </h5>
-                    <h6>Por <a href="">Yasmin</a>  28/10/2022</h6>
-                </div>
-            </div>
+            <h4 class="cor-letra">
+            Jennifer Lawrence descarta papéis em grandes franquias: "Velha demais"
+            </h4>
+            <h5 class="cor-letra-D2D2D2">
+            Estrela de X-Men e Jogos Vorazes deve se concentrar em títulos independentes
+            </h5>
+            <h6>
+              Por <a href="../autores.php?#caio-coletti">Caio Coletti</a>  08/10/20222
+            </h6>
+            <!-- https://www.omelete.com.br/autores/caio-coletti-->
             <p>
-            Lorem ipsum dolor sit amet, consecteturLorem ipsum
-            dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
-            consecteturLorem ipsum dolor sit amet,
+            Não espere ver Jennifer Lawrence de volta às grandes franquias hollywoodianas. Participando do BFI London Film Festival [via Deadline], ela explicou por que deve se concentrar em projetos independentes daqui em diante.
             </p>
-          </div>
-          <div class="text-end">
-            <a href="">Ler Mais</a>
+            <div class="text-end">
+            <a href="artigo-literatura.php?cod=3">Ler Mais</a>
+            </div>
           </div>
         </section>
         <section class="col-12 col-sm-6 col-lg-4 p-1">
-          <div class="text-center">
-            <picture>
-              <source srcset="https://via.placeholder.com/113x154" media="(min-width:992px)">
-              <img src="https://via.placeholder.com/250x250" alt="Capa Livro" >
-            </picture>
-            <h2>Filme</h2>
+        <div class="text-center">
+            <figure>
+              <img src="img/harry-potter.jpg" alt="Capa Livro">
+              <figcaption>Harry Potter</figcaption>
+            </figure>
           </div>
           <div class="text-left">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h5>Lorem ipsum dolor sit amet, consectetur </h5>
-                    <h6>Por <a href="">Yasmin</a>  28/10/2022</h6>
-                </div>
-            </div>
+            <h5 class="cor-letra">
+            Harry Potter é a a saga do órfão bruxo criada por J.K. Rowling e iniciada em 1997.
+            </h5>
+            <h6>
+              Por <a href="../autores.php/#omelete">Omelete</a>  04/12/20222
+            </h6>
+            <!-- https://www.omelete.com.br/harry-potter?gclid=EAIaIQobChMI3Kaew5_g-wIVFWSRCh0sSAh2EAAYASAAEgLR_vD_BwE -->
             <p>
-            Lorem ipsum dolor sit amet, consecteturLorem ipsum
-            dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
-            consecteturLorem ipsum dolor sit amet,
+            Depois dos oito filmes baseados nos livros, a franquia agora se prepara para 
+            lançar Animais Fantásticos e Onde Habitam, que adapta o livro didático 
+            no mundo de Harry Potter que cataloga 75 espécies de criaturas 
+            mágicas pelos cinco continentes.
             </p>
-          </div>
-          <div class="text-end">
-            <a href="">Ler Mais</a>
+            <div class="text-end">
+            <a href="artigo-literatura.php?cod=2">Ler Mais</a>
+            </div>
           </div>
         </section>
-        <section class="col-12 col-sm-6 col-lg-4 p-1">
-          <div class="text-center">
-            <picture>
-              <source srcset="https://via.placeholder.com/113x154" media="(min-width:992px)">
-              <img src="https://via.placeholder.com/250x250" alt="Capa Livro">
-            </picture>
-            <h2>Filme</h2>
+        <section class="col-12 col-sm-12 col-lg-12 p-1">
+        <div class="text-center">
+            <figure>
+              <img src="img/artigos/banner-senhor-dos-aneis.jpg" alt="Capa Livro">
+              <figcaption>Senhor dos Anéis</figcaption>
+            </figure>
           </div>
           <div class="text-left">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h5>Lorem ipsum dolor sit amet, consectetur </h5>
-                      <h6>Por <a href="">Yasmin</a>  28/10/2022</h6>
-                </div>
-            </div>
+            <h4 class="cor-letra">
+                Os Anéis de Poder | Prime Video anuncia novas escalações do elenco; veja
+            </h4>
+            <h5 class="cor-letra-D2D2D2">
+            Personagens dos atores não foram revelados
+            </h5>
+            <h6>
+              Por <a href="../autores.php?flavio-pinto">Flávio Pinto</a>  28/10/20222
+            </h6>
+            <!-- https://www.omelete.com.br/autores/flavio-pinto-->
             <p>
-            Lorem ipsum dolor sit amet, consecteturLorem ipsum
-            dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
-            consecteturLorem ipsum dolor sit amet,
+            O Prime Video aproveitou a manhã de hoje (1) para anunciar as novas adições do elenco de O Senhor dos Anéis: Os Anéis de Poder. Em formato de fio, a plataforma, aos poucos, revelou o nome dos 7 atores que se juntarão à produção na a próxima temporada. 
             </p>
-          </div>
-          <div class="text-end">
-            <a href="">Ler Mais</a>
-          </div>
-        </section>
-        <section class="col-12 col-sm-6 col-lg-4 p-1">
-          <div class="text-center">
-            <picture>
-              <source srcset="https://via.placeholder.com/113x154" media="(min-width:992px)">
-              <img src="https://via.placeholder.com/250x250" alt="Capa Livro">
-            </picture>
-            <h2>Filme</h2>
-          </div>
-          <div class="text-left">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h5>Lorem ipsum dolor sit amet, consectetur </h5>
-                      <h6>Por <a href="">Yasmin</a>  28/10/2022</h6>
-                </div>
+            <div class="text-end">
+            <a href="artigo-literatura.php?cod=4">Ler Mais</a>
             </div>
-            <p>
-            Lorem ipsum dolor sit amet, consecteturLorem ipsum
-            dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
-            consecteturLorem ipsum dolor sit amet,
-            </p>
           </div>
-          <div class="text-end">
-            <a href="">Ler Mais</a>
-          </div>
-        </section>
-        <section class="col-12 col-sm-6 col-lg-4 p-1">
-          <div class="text-center">
-            <picture>
-              <source srcset="https://via.placeholder.com/113x154" media="(min-width:992px)">
-              <img src="https://via.placeholder.com/250x250" alt="Capa Livro">
-            </picture>
-            <h2>Filme</h2>
-          </div>
-          <div class="text-left">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h5>Lorem ipsum dolor sit amet, consectetur </h5>
-                      <h6>Por <a href="">Yasmin</a>  28/10/2022</h6>
-                </div>
-            </div>
-            <p>
-            Lorem ipsum dolor sit amet, consecteturLorem ipsum
-            dolor sit amet, consectetur Lorem ipsum dolor sit amet, 
-            consecteturLorem ipsum dolor sit amet,
-            </p>
-          </div>
-          <div class="text-end">
-            <a href="">Ler Mais</a>
-          </div>
+        </div>
+      </div>
         </section>
       </div>
     </div>
