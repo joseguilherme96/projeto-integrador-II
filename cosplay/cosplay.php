b9b1ddfb8456bebf01105f281669cab3daa8d4f2
MelissaGodoy <melissaraineri328@gmail.com>
2022-12-18 15:55:24 -0300

    arrumando links e identacao das paginas cosplay

--- cosplay/cosplay.php
+++ cosplay/cosplay.php
@@ -1,83 +1,76 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <title>Cosplay</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link href="/projeto-integrador-II/css//tema.css" rel="stylesheet" >  
+<head>
+  <title>Cosplay</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link href="../css/tema.css" rel="stylesheet">
 </head>
 <body>
-<?php include "../nav.php"?>
+  <?php include "../nav.php" ?>
   <div class="container">
-   
     <div class="col-12 text-center pt-5">
-    <h1 class="cor-letra"> COSPLAY </h1>
+      <h1 class="cor-letra"> COSPLAY </h1>
     </div>
-    <section class="row pt-5 "> 
-      
-      <div class="col-12 col-sm-6 col-lg-3 text-center pt-5 "> 
-      <a href="https://www.instagram.com/p/ClEcb9ojfda/?utm_source=ig_embed&ig_rid=ec94feb5-a4f1-49f9-bc25-1625c4ae9ef5"><img src="/projeto-integrador-II/imgcosplay//kodia.jpg " alt="Ahsoka Tano - Star Wars
-Incrivel Cosplay do personagem Ahsoka Tano feita por uma fã da grande série Star Wars surpreendeu nas redes sociais, a fã descreve *Esse cosplay foi um verdadeiro desafio pra mim, principalmente a maquiagem, e aprendi muito fazendo.Mal posso esperar para usá-la na Star Wars Celebration em Londres no ano que vem." > </a>
-      </div>  
-
-      <div class="col-12 col-sm-6 col-lg-3 pt-5 pb-5 text-start "> 
-        <p> <h6 class="cor-letra"> Ahsoka Tano - Star Wars </h6> Incrivel Cosplay do personagem "Ahsoka Tano" feita por uma fã da grande série Star Wars surpreendeu nas redes sociais, a fã descreve "Esse cosplay foi um verdadeiro desafio pra mim, principalmente a maquiagem, e aprendi muito fazendo.Mal posso esperar para usá-la na Star Wars Celebration em Londres no ano que vem. 
-        </p>
+    <section class="row pt-5 ">
+      <div class="col-12 col-sm-6 col-lg-3 text-center pt-5 ">
+        <a href="https://www.instagram.com/p/ClEcb9ojfda/?utm_source=ig_embed&ig_rid=ec94feb5-a4f1-49f9-bc25-1625c4ae9ef5"><img src=" imgcosplay/kodia.jpg " alt="Ahsoka Tano - Star Wars
+Incrivel Cosplay do personagem Ahsoka Tano feita por uma fã da grande série Star Wars surpreendeu nas redes sociais, a fã descreve *Esse cosplay foi um verdadeiro desafio pra mim, principalmente a maquiagem, e aprendi muito fazendo.Mal posso esperar para usá-la na Star Wars Celebration em Londres no ano que vem."> </a>
       </div>
-   
-      <div class="col-12 col-sm-6 col-lg-3 pt-5 text-center"><a href="https://www.instagram.com/jennifercosplays/?utm_source=ig_embed&ig_rid=fc50bd0c-6612-4040-a0c9-3b7372100f2c"> <img src="/projeto-integrador-II/imgcosplay//arlequina.jpg" alt="Deadpool e Arlequina
+      <div class="col-12 col-sm-6 col-lg-3 pt-5 pb-5 text-start ">
+        <h6 class="cor-letra"> Ahsoka Tano - Star Wars </h6>
+        <p>Incrivel Cosplay do personagem "Ahsoka Tano" feita por uma fã da grande série Star Wars surpreendeu nas redes sociais, a fã descreve "Esse cosplay foi um verdadeiro desafio pra mim, principalmente a maquiagem, e aprendi muito fazendo.Mal posso esperar para usá-la na Star Wars Celebration em Londres no ano que vem.</p>
+      </div>
+      <div class="col-12 col-sm-6 col-lg-3 pt-5 text-center"><a href="https://www.instagram.com/jennifercosplays/?utm_source=ig_embed&ig_rid=fc50bd0c-6612-4040-a0c9-3b7372100f2c"> <img src=" imgcosplay/arlequina.jpg" alt="Deadpool e Arlequina
 Deadpool e Arlequina juntos em incrivel cosplay feito por fã, quem conhece sabe que os dois personagem são de casas de super-herois diferentes, então essa amizade é bem improvavel acontecer um dia, em algum filme, mas os fãs quebram barreiras. "> </a>
-      </div>  
-
-      <div class="col-12 col-sm-6 col-lg-3  pt-5 pb-5 text-start " > 
-        <p> <h6 class="cor-letra">Deadpool e Arlequina </h6> Deadpool e Arlequina juntos em incrivel cosplay feito por fã, quem conhece sabe que os dois personagem são de casas de super-herois diferentes, então essa amizade é bem improvavel acontecer um dia, em algum filme, mas os fãs quebram barreiras. </p>
       </div>
-
+      <div class="col-12 col-sm-6 col-lg-3  pt-5 pb-5 text-start ">
+        <h6 class="cor-letra">Deadpool e Arlequina </h6>
+        <p>Deadpool e Arlequina juntos em incrivel cosplay feito por fã, quem conhece sabe que os dois personagem são de casas de super-herois diferentes, então essa amizade é bem improvavel acontecer um dia, em algum filme, mas os fãs quebram barreiras. </p>
+      </div>
     </section>
-     
-    <section class="row pt-5 "> 
-
-      <div class="col-12 col-sm-6 col-lg-3 pt-5 text-center" > <a href="https://animenew.com.br/god-of-war-ragnarok-cosplay-impecavel-deixa-fas-surpresos/"><img src="/projeto-integrador-II/imgcosplay//gowcosplay.jpg" alt="God of War Ragnarök
+    <section class="row pt-5 ">
+      <div class="col-12 col-sm-6 col-lg-3 pt-5 text-center"> <a href="https: //animenew.com.br/god-of-war-ragnarok-cosplay-impecavel-deixa-fas-surpresos/"><img src=" imgcosplay /gowcosplay.jpg" alt="God of War Ragnarök
 Cosplay surpreende fãs da grande franquia, com o recém lançamendo do jogo God of War Ragnarök, que está sendo um sucesso absurdo, aclamada em todo o mundo, alguns comentarios positivos surgiram como este *A equipe envolvida na construção desses cosplays é simplesmente incrível*"> </a>
       </div>
 
-      <div class="col-12 col-sm-6 col-lg-3 pt-5 pb-5 text-start" > 
-        <p> <h6 class="cor-letra">God of War Ragnarök</h6> Cosplay surpreende fãs da grande franquia, com o recém lançamendo do jogo God of War Ragnarök, que está sendo um sucesso absurdo, aclamada em todo o mundo, alguns comentarios positivos surgiram como este "A equipe envolvida na construção desses cosplays é simplesmente incrível". </p> 
+      <div class="col-12 col-sm-6 col-lg-3 pt-5 pb-5 text-start">
+        <h6 class="cor-letra">God of War Ragnarök</h6>
+        <p>Cosplay surpreende fãs da grande franquia, com o recém lançamendo do jogo God of War Ragnarök, que está sendo um sucesso absurdo, aclamada em todo o mundo, alguns comentarios positivos surgiram como este "A equipe envolvida na construção desses cosplays é simplesmente incrível". </p>
       </div>
 
 
-      <div class="col-12 col-sm-6 col-lg-3 pt-5 text-center" > <a href="https://twitter.com/omelete/status/1144281826183327744"><img src="/projeto-integrador-II/imgcosplay//capitaoamerica.jpeg" alt="Capitão America
+      <div class="col-12 col-sm-6 col-lg-3 pt-5 text-center"> <a href="https: //twitter.com/omelete/status/1144281826183327744"><img src=" imgcosplay /capitaoamerica.jpeg" alt="Capitão America
 Incrivel cosplay de capitão america surpreende na CCXP, diretamente de guerra infinita, um dos herois mais queridos do mundo dos cinemas."> </a>
       </div>
-    
-      <div class="col-12 col-sm-6 col-lg-3 pt-5 pb-5 text-start " > 
-        <p> <h6 class="cor-letra" >Capitão America </h6> Incrivel cosplay de capitão america surpreende na CCXP, diretamente de guerra infinita, um dos herois mais queridos do mundo dos cinemas.</p>
+
+      <div class="col-12 col-sm-6 col-lg-3 pt-5 pb-5 text-start ">
+        <h6 class="cor-letra">Capitão America </h6>
+        <p>Incrivel cosplay de capitão america surpreende na CCXP, diretamente de guerra infinita, um dos herois mais queridos do mundo dos cinemas.</p>
       </div>
-
     </section>
-
     <section class="row pt-5 ">
-
-      <div class="col-12 col-sm-6 col-lg-3 pt-5 text-center" > 
-      <a href="https://www.instagram.com/jennifercosplays/?utm_source=ig_embed&ig_rid=fc50bd0c-6612-4040-a0c9-3b7372100f2c"><img src="/projeto-integrador-II/imgcosplay//gueen.jpg" alt="Gueen
+      <div class="col-12 col-sm-6 col-lg-3 pt-5 text-center">
+        <a href="https://www.instagram.com/jennifercosplays/?utm_source=ig_embed&ig_rid=fc50bd0c-6612-4040-a0c9-3b7372100f2c"><img src=" imgcosplay/gueen.jpg" alt="Gueen
 Fã faz cosplay de Gueen, uma das personagens mais amada da serie de filmes do Homem Aranha, além desses a fã ja fez diversos outros personagens."> </a>
       </div>
-    
-      <div class="col-12 col-sm-6 col-lg-3 pt-5 pb-5 text-start" > 
-        <p> <h6 class="cor-letra">Gueen</h6> Fã faz cosplay de Gueen, uma das personagens mais amada da serie de filmes do Homem Aranha, além desses a fã ja fez diversos outros personagens.</p>
+
+      <div class="col-12 col-sm-6 col-lg-3 pt-5 pb-5 text-start">
+        <h6 class="cor-letra">Gueen</h6>
+        <p> Fã faz cosplay de Gueen, uma das personagens mais amada da serie de filmes do Homem Aranha, além desses a fã ja fez diversos outros personagens.</p>
       </div>
 
-      <div class="col-12 col-sm-6 col-lg-3 pt-5 pb-5 text-center" > <a href="https://www.instagram.com/jennifercosplays/?utm_source=ig_embed&ig_rid=fc50bd0c-6612-4040-a0c9-3b7372100f2c"> <img src="/projeto-integrador-II/imgcosplay//sylvie.jpg" alt="Sylvie
+      <div class="col-12 col-sm-6 col-lg-3 pt-5 pb-5 text-center"> <a href="https://www.instagram.com/jennifercosplays/?utm_source=ig_embed&ig_rid=fc50bd0c-6612-4040-a0c9-3b7372100f2c"> <img src=" imgcosplay/sylvie.jpg" alt="Sylvie
 Quem ai conhece a grande *vilã* da serie Loki? Sylvie, uma personagem bastante imponente e que faz parte de um dos grandes estopims da marvel."></a>
       </div>
-        
-      <div class="col-12 col-sm-6 col-lg-3 pt-5 pb-5 text-start"> 
-        <p> <h6 class="cor-letra">Sylvie</h6> Quem ai conhece a grande "vilã" da serie Loki? Sylvie, uma personagem bastante imponente e que faz parte de um dos grandes estopims da marvel.   </p> 
-      </div>
 
+      <div class="col-12 col-sm-6 col-lg-3 pt-5 pb-5 text-start">
+        <h6 class="cor-letra">Sylvie</h6>
+        <p> Quem ai conhece a grande "vilã" da serie Loki? Sylvie, uma personagem bastante imponente e que faz parte de um dos grandes estopims da marvel. </p>
+      </div>
     </section>
   </div>
-  <?php include "../rodape.php"?>
+  <?php include "../rodape.php" ?>
 </body>
 </html>
