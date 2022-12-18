ed76dd735a69c49ceff43f2afd533bd91406e3b9
MelissaGodoy <melissaraineri328@gmail.com>
2022-12-18 15:09:42 -0300

    arrumando as fotos e os links da pagina quem somos

--- quemsomos.php
+++ quemsomos.php
@@ -11,65 +11,55 @@
 <body>
 <?php include "nav.php"?>
   <div class="container">
-    
     <section class="row pt-5 "> 
-    <section class="text-center cor-fundo">
-  <i class="bi bi-square-fill fs-1 d-inline cor-letra"></i>
-    <h1 class="d-inline cor-letra">QUEM SOMOS</h1>
-</section>
+      <section class="text-center cor-fundo">
+        <i class="bi bi-square-fill fs-1 d-inline cor-letra"></i>
+        <h1 class="d-inline cor-letra">QUEM SOMOS</h1>
+      </section>
       <div class="col-12 col-sm-6 col-lg-6 text-center pt-5 cor-fundo"> 
-      <img src="/projeto-integrador-II/jose1.jpg" class="img-fluid "alt="Aqui temos a imagem do José" >
-      </div>  
-
+        <img src="img/jose1.jpg" class="img-fluid "alt="Aqui temos a imagem do José" >
+      </div> 
       <div class="col-12 col-sm-6 col-lg-6 pt-5 pb-5 text-start cor-fundo"> 
-      <p>José Guilherme Guimãres Campos:</p>
-      <p>RA: 2650832213006</p>
-      <p>Biografia: Integrante do grupo Pirulito Malignos de açucar e mel, reside em Mairinque, tem 25 anos e está cursando o segundo Semestre de Sistemas para internet na FATEC de São Roque. </p>
-      <p>Função no Grupo: Gerente</p>
-      <p>Responsável pelo desenvovimento das seguintes páginas relacionadas a cinema : </p>
+        <p>José Guilherme Guimãres Campos:</p>
+        <p>RA: 2650832213006</p>
+        <p>Biografia: Integrante do grupo Pirulito Malignos de açucar e mel, reside em Mairinque, tem 25 anos e está cursando o segundo Semestre de Sistemas para internet na FATEC de São Roque. </p>
+        <p>Função no Grupo: Gerente</p>
+        <p>Responsável pelo desenvovimento das seguintes páginas relacionadas a cinema : </p>
         <ul class="list-unstyled">
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/cinema/index.php">Cinema</a> </li>
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/cinema/entrevistas.php">Entrevistas</a> </li>
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/cinema/filmes.php">Filmes</a> </li>
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/cinema/literatura.php">Literatura</a> </li>
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/cinema/streaming.php">Streaming</a> </li>
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/contato.php">Contato</a> </li>
-          </ul> 
-          
+          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="cinema/index.php">Cinema</a> </li>
+          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="cinema/entrevistas.php">Entrevistas</a> </li>
+          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="cinema/filmes.php">Filmes</a> </li>
+          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="cinema/literatura.php">Literatura</a> </li>
+          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="cinema/streaming.php">Streaming</a> </li>
+          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="contato.php">Contato</a> </li>
+        </ul>   
       </div>
-      <div class="row pt-5">
+    </section>
+    <section class="row pt-5">
       <div class="col-12 col-sm-6 col-lg-6 text-center cor-fundo pt-5 "> 
-        <img src="/projeto-integrador-II/melissa.jpg.jpg" class="img-fluid " alt="Aqui temos a imagem da Melissa.">
+        <img src="img/melissa.jpg.jpg" class="img-fluid " alt="Aqui temos a imagem da Melissa.">
       </div>  
-
-    
       <div class="col-12 col-sm-6 col-lg-6 text-start cor-fundo py-5 " > 
-      
         <p> Melissa Raineri Godoy:</p>
         <p>RA: 2650832213005 </p>
         <p>Biografia: Integrante do grupo Pirulito Malignos de açucar e mel, reside em Vargem Grande Paulista, tem 23 anos e está cursando o segundo Semestre de Sistemas para internet na FATEC de São Roque. </p>
         <p>Função no Grupo: Designer</p>
         <p>Responsável pelo desenvolvimento das seguintes páginas relacionadas a Series e TV : </p>
         <ul class="list-unstyled">
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/Seriesetv.php/atores.php">Atores</a> </li>
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/Seriesetv.php/criticas.php">Criticas</a> </li>
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/Seriesetv.php/lançamentos.php">Lançamentos</a> </li>
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/Seriesetv.php/resenha.php">Resenha</a> </li>
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/Seriesetv.php/noticias.php">Noticias</a> </li>
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/Seriesetv.php/serieshome.php">Series e TV</a> </li>
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/autores.php">Autores</a> </li>
-          </ul> 
-          
+          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="Seriesetv.php/atores.php">Atores</a> </li>
+          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="Seriesetv.php/criticas.php">Criticas</a> </li>
+          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="Seriesetv.php/lançamentos.php">Lançamentos</a> </li>
+          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="Seriesetv.php/resenha.php">Resenha</a> </li>
+          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="Seriesetv.php/noticias.php">Noticias</a> </li>
+          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="Seriesetv.php/serieshome.php">Series e TV</a> </li>
+          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="autores.php">Autores</a> </li>
+        </ul>  
       </div>
-</div>
-   
-     
+    </section> 
     <section class="row pt-5 "> 
-
       <div class="col-12 col-sm-6 col-lg-6 text-center cor-fundo cor-fundo py-5" > 
-        <img src="/projeto-integrador-II/gilberto.jpg" class="img-fluid " alt="Aqui temos a imagem do Gilberto">
+        <img src="img/gilberto.jpg" class="img-fluid " alt="Aqui temos a imagem do Gilberto">
       </div>
-
       <div class="col-12 col-sm-6 col-lg-6 text-start cor-fundo py-5" > 
         <p> Gilberto Giliarde Bueno Camargo :</p>
         <p>RA: 2650832213006 </p>
@@ -77,40 +67,36 @@
         <p>Função no Grupo: Coder</p>
         <p>Responsável pelo desenvolvimento das seguintes páginas relacionadas a Cosplay : </p>
         <ul class="list-unstyled">
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/cosplay/animes.php">Animes</a> </li>
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/cosplay/cosplay.php">Cosplay</a> </li>
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/cosplay/eventos.php">Eventos</a> </li>
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/cosplay/games.php">Games</a> </li>
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/cosplay/noticias.php">Noticias</a> </li>
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/normasdeutilização.php">Normas de Utilização</a> </li>
+          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="cosplay/animes.php">Animes</a> </li>
+          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="cosplay/cosplay.php">Cosplay</a> </li>
+          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="cosplay/eventos.php">Eventos</a> </li>
+          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="cosplay/games.php">Games</a> </li>
+          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="cosplay/noticias.php">Noticias</a> </li>
+          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="normasdeutilização.php">Normas de Utilização</a> </li>
           </ul> 
       </div>
-</section>
-<section class="row pt-5 pb-5">
-
+    </section>
+    <section class="row pt-5 pb-5">
       <div class="col-12 col-sm-6 col-lg-6 text-center cor-fundo py-5" > 
-        <img src="/projeto-integrador-II/yasmin1.jpg" class="img-fluid " alt="Aqui temos a imagem da Yasmin">
+        <img src="img/yasmin1.jpg" class="img-fluid " alt="Aqui temos a imagem da Yasmin">
       </div>
-    
       <div class="col-12 col-sm-6 col-lg-6  text-start cor-fundo py-5" > 
         <p>Yasmin do Nascimento :</p>
         <p>RA: 2650832213008 </p>
         <p>Biografia: Integrante do grupo Pirulito Malignos de açucar e mel, reside em Vargem Grande Paulista, tem 18 anos e está cursando o segundo Semestre de Sistemas para internet na FATEC de São Roque. </p>
-          <p>Função no Grupo: Coder</p>
-          <p>Responsavél pelo desenvolvimento das seguintes páginas relacionadas a HQS :</p>
-          <ul class="list-unstyled">
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/HQS/autores.php">Autores</a> </li>
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/HQS/critica.php">Criticas</a> </li>
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/HQS/pg.php">HQS</a> </li>
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/HQS/topfive.php">Top Five</a> </li>
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/HQS/ultimasnoticias.php">Noticias</a> </li>
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/HQS/materia.php">Materia</a> </li>
-          <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="/projeto-integrador-II/quemsomos.php">Quem Somos</a> </li>
-
+        <p>Função no Grupo: Coder</p>
+        <p>Responsavél pelo desenvolvimento das seguintes páginas relacionadas a HQS :</p>
+        <ul class="list-unstyled">
+        <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="HQS/autores.php">Autores</a></li>
+        <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="HQS/critica.php">Criticas</a></li>
+        <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="HQS/pg.php">HQS</a></li>
+        <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="HQS/topfive.php">Top Five</a></li>
+        <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="HQS/ultimasnoticias.php">Noticias</a></li>
+        <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="HQS/materia.php">Materia</a></li>
+        <li><i class="bi bi-square-fill fs-6 d-inline cor-letra"></i> <a href="quemsomos.php">Quem Somos</a></li>
+      </div>
     </section>
-
   </div>
-<footer><?php include "rodape.php"?></footer>
-  
+  <footer><?php include "rodape.php"?></footer> 
 </body>
 </html>
