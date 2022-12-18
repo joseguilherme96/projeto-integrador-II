27184cc7156332ecab97868cfea371b2dd6ed369
joseguilherme96 <jose.guilherme96@outlook.com.br>
2022-12-18 13:27:00 -0300

    Atualizado endereços com a mudança da pasta para projeto-integrador-main

--- rodape.php
+++ rodape.php
@@ -6,40 +6,40 @@
 <footer class="row border text-light">
                 <div class="col-sm-3 col-lg-1 d-none d-sm-inline">
                     <ul>
-                        <li><a href="/projeto-integrador-II/cinema/index.php" class="text-decoration-none text-light">Cinema</a></li>
-                        <li><a href="/projeto-integrador-II/cinema/filmes.php" class="text-decoration-none text-light" >Filmes</a></li>
-                        <li><a href="/projeto-integrador-II/cinema/streaming.php" class="text-decoration-none text-light">Streaming</a></li>
-                        <li><a href="/projeto-integrador-II/cinema/literatura.php" class="text-decoration-none text-light">Literatura</a></li>
-                        <li><a href="/projeto-integrador-II/cinema/entrevistas.php" class="text-decoration-none text-light">Entrevistas</a></li>
+                        <li><a href="/projeto-integrador-II-main/cinema/index.php" class="text-decoration-none text-light">Cinema</a></li>
+                        <li><a href="/projeto-integrador-II-main/cinema/filmes.php" class="text-decoration-none text-light" >Filmes</a></li>
+                        <li><a href="/projeto-integrador-II-main/cinema/streaming.php" class="text-decoration-none text-light">Streaming</a></li>
+                        <li><a href="/projeto-integrador-II-main/cinema/literatura.php" class="text-decoration-none text-light">Literatura</a></li>
+                        <li><a href="/projeto-integrador-II-main/cinema/entrevistas.php" class="text-decoration-none text-light">Entrevistas</a></li>
                     </ul>
                 </div>
                 <div class="col-sm-3 col-lg-1 d-none d-sm-inline">
                     <ul >
-                        <li><a href="/projeto-integrador-II/HQS/index.php" class="text-decoration-none text-light">HQS</a></li>
-                        <li><a href="/projeto-integrador-II/HQS/materia.php" class="text-decoration-none text-light">Matérias</a></li>
-                        <li><a href="/projeto-integrador-II/HQS/topfive.php" class="text-decoration-none text-light">Top Five</a></li>
-                        <li><a href="/projeto-integrador-II/HQS/ultimasnoticias.php" class="text-decoration-none text-light">Melhores Criticas</a></li>
-                        <li><a href="/projeto-integrador-II/HQS/autores.php" class="text-decoration-none text-light">Melhores Autores</a></li>
-                        <li><a href="/projeto-integrador-II/HQS/critica.php" class="text-decoration-none text-light">Criticas</a></li>
+                        <li><a href="/projeto-integrador-II-main/HQS/pg.php" class="text-decoration-none text-light">HQS</a></li>
+                        <li><a href="/projeto-integrador-II-main/HQS/materia.php" class="text-decoration-none text-light">Matérias</a></li>
+                        <li><a href="/projeto-integrador-II-main/HQS/topfive.php" class="text-decoration-none text-light">Top Five</a></li>
+                        <li><a href="/projeto-integrador-II-main/HQS/ultimasnoticias.php" class="text-decoration-none text-light">Melhores Criticas</a></li>
+                        <li><a href="/projeto-integrador-II-main/HQS/autores.php" class="text-decoration-none text-light">Melhores Autores</a></li>
+                        <li><a href="/projeto-integrador-II-main/HQS/critica.php" class="text-decoration-none text-light">Criticas</a></li>
                     </ul>
                 </div>
                 <div class="col-sm-3 col-lg-1 d-none d-sm-inline">
                     <ul >
-                        <li><a href="/projeto-integrador-II/cosplay/cosplay.php" class="text-decoration-none text-light">Cosplay</a></li>
-                        <li><a href="/projeto-integrador-II/cosplay/animes.php" class="text-decoration-none text-light">Animes</a></li>
-                        <li><a href="/projeto-integrador-II/cosplay/games.php" class="text-decoration-none text-light">Game</a></li>
-                        <li><a href="/projeto-integrador-II/cosplay/noticias.php" class="text-decoration-none text-light">Noticias</a></li>
-                        <li><a href="/projeto-integrador-II/cosplay/eventos.php" class="text-decoration-none text-light">Eventos</a></li>
+                        <li><a href="/projeto-integrador-II-main/cosplay/cosplay.php" class="text-decoration-none text-light">Cosplay</a></li>
+                        <li><a href="/projeto-integrador-II-main/cosplay/animes.php" class="text-decoration-none text-light">Animes</a></li>
+                        <li><a href="/projeto-integrador-II-main/cosplay/games.php" class="text-decoration-none text-light">Game</a></li>
+                        <li><a href="/projeto-integrador-II-main/cosplay/noticias.php" class="text-decoration-none text-light">Noticias</a></li>
+                        <li><a href="/projeto-integrador-II-main/cosplay/eventos.php" class="text-decoration-none text-light">Eventos</a></li>
                     </ul>
                 </div>
                 <div class="col-sm-3  col-lg-1 d-none d-sm-inline">
                     <ul >
-                        <li><a class="dropdown-item" href="/projeto-integrador-II/Seriesetv.php/serieshome.php" class="text-decoration-none text-light">Serie/Tv</a></li>
-                        <li><a class="dropdown-item" href="/projeto-integrador-II/Seriesetv.php/resenha.php" class="text-decoration-none text-light">Resenhas</a></li>
-                        <li><a class="dropdown-item" href="/projeto-integrador-II/Seriesetv.php/lançamentos.php" class="text-decoration-none text-light">Lançamentos</a></li>
-                        <li><a class="dropdown-item" href="/projeto-integrador-II/Seriesetv.php/criticas.php" class="text-decoration-none text-light">Criticas</a></li>
-                        <li><a class="dropdown-item" href="/projeto-integrador-II/Seriesetv.php/noticias.php" class="text-decoration-none text-light">Notícias</a></li>
-                        <li><a class="dropdown-item" href="/projeto-integrador-II/Seriesetv.php/atores.php" class="text-decoration-none text-light">Atores</a></li>
+                        <li><a class="dropdown-item" href="/projeto-integrador-II-main/Seriesetv.php/serieshome.php" class="text-decoration-none text-light">Serie/Tv</a></li>
+                        <li><a class="dropdown-item" href="/projeto-integrador-II-main/Seriesetv.php/resenha.php" class="text-decoration-none text-light">Resenhas</a></li>
+                        <li><a class="dropdown-item" href="/projeto-integrador-II-main/Seriesetv.php/lançamentos.php" class="text-decoration-none text-light">Lançamentos</a></li>
+                        <li><a class="dropdown-item" href="/projeto-integrador-II-main/Seriesetv.php/criticas.php" class="text-decoration-none text-light">Criticas</a></li>
+                        <li><a class="dropdown-item" href="/projeto-integrador-II-main/Seriesetv.php/noticias.php" class="text-decoration-none text-light">Notícias</a></li>
+                        <li><a class="dropdown-item" href="/projeto-integrador-II-main/Seriesetv.php/atores.php" class="text-decoration-none text-light">Atores</a></li>
                     </ul>
                 </div>
                 <div class="col-3 col-lg-1">
@@ -49,25 +49,25 @@
                    <div class="row">
                         <div class="col-3 col-lg-3">
                             <figure>
-                                <img src="/projeto-integrador-II/img/IMG-20221204-WA0101.jpg" class="img-fluid" >
+                                <img src="/projeto-integrador-II-main/img/IMG-20221204-WA0101.jpg" class="img-fluid" >
                                 <figcaption class="text-center" >Gilberto</figcaption>
                             </figure>
                         </div>
                         <div class="col-3 col-lg-3 ">
                         <figure>
-                                <img src="/projeto-integrador-II/img/IMG_20220922_071956875.jpg" class="img-fluid">
+                                <img src="/projeto-integrador-II-main/img/IMG_20220922_071956875.jpg" class="img-fluid">
                                 <figcaption class="text-center">José Guilherme</figcaption>
                             </figure>
                         </div>
                         <div class="col-3 col-lg-3">
                         <figure>
-                                <img src="/projeto-integrador-II/img/Screenshot_20221204-094744.png" class="img-fluid">
+                                <img src="/projeto-integrador-II-main/img/Screenshot_20221204-094744.png" class="img-fluid">
                                 <figcaption class="text-center">Melissa</figcaption>
                             </figure>
                         </div>
                         <div class="col-3 col-lg-3">
                             <figure>
-                                <img src="/projeto-integrador-II/img/IMG-20221204-WA0097.jpg" class="img-fluid">
+                                <img src="/projeto-integrador-II-main/img/IMG-20221204-WA0097.jpg" class="img-fluid">
                                 <figcaption class="text-center">Yasmin</figcaption>
                             </figure>
                         </div>
