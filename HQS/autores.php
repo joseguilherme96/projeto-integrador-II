1e9b2da69ea49f5604c9f4c5da1f9abd16ac3b80
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2023-04-25 08:23:38 -0300

    correção yasmin

--- HQS/autores.php
+++ HQS/autores.php
@@ -1,125 +1,80 @@
-<!doctype html>
-<HTML lang="PT-BR">
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>HQS</title>
-  <link href="../css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-  <link rel="stylesheet" href="../node_modules/bootstrap-icons/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="../css/tema.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="../node_modules/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="../css/tema.css">
+    <title>Séries e TV</title>
 </head>
-
 <body>
-<main>
-
-  <nav>
-  <?php include "../nav.php" ?>
-</nav>
-
-<section>
-  <h1>Autores </h1>
-
-  <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
-
-       <div class="carousel-inner">
-         <div class="carousel-item active">
-          <figure>
-         <img src=" ../HQS/img/a22.jpg" class="d-block w-100" alt="Quadrinhos">
-           <figcaption> quadrinhos</figcaption>
-            </figure>
-               <div class="carousel-caption d-none d-md-block">
-                 </div>
-                   </div>
-                      <div class="carousel-item">
-                          <figure>
-                               <img src=" ../HQS/img/a23.jpg" class="d-block w-100" alt="Quadrinhos">
-                                  <figcaption> quadrinhos</figcaption>
-                                      </figure>
-                                         <div class="carousel-caption d-none d-md-block">
-                                            </div>
-                                               </div>
-                                                  <div class="carousel-item">
-                                               <figure>
-                                              <img src=" ../HQS/img/a24.jpg" class="d-block w-100" alt="Quadrinhos">
-                                          <figcaption> quadrinhos</figcaption>
-                                       </figure>
-                                     <div class="carousel-caption d-none d-md-block">
-                                  </div>
-                               </div>
-                             </div>
-                           <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Prev</span>
-                  </button>
-               <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-           <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
-
-</section>
-
-  <section class="row pt-5 ">
-    
-  <div class="container">
-
-
-    <h1>Autores</h1>
-
-
-<article>
-
-      <div class="col-12 col-sm-12 col-lg-6 text-center pt-5 ">
-
-        <figure>
-        <img src=" ../HQS/img/a1.jpg" class="img-fluid" alt="Aqui temos a imagem do Mauricio de Sousa">
-        <figcaption> Mauricio de Sousa </figcaption>
-        </figure>
-      </div>
-
-      <div class="col-12 col-sm-12 col-lg-6 pt-5 pb-5 text-start ">
-        <div class="cor-fundo">
-          <h2 class="cor-letra">Mauricio de Sousa</h2>
-          <p>Mauricio de Sousa é desenhista, empresário, o mais premiado autor brasileiro de quadrinhos e membro da Academia Paulista de Letras. Nasceu em 27 de outubro de 1935, numa família de poetas e contadores de histórias em Santa Isabel, no interior de São Paulo. Ainda criança, mudou-se para Mogi das Cruzes, onde descobriu sua paixão pelo desenho e começou a criar os primeiros personagens. Com 19 anos, foi para São Paulo tentar trabalhar como ilustrador na Folha da Manhã (hoje Folha de S.Paulo). No entanto, conseguiu apenas uma vaga de repórter policial.
-            Em 1959, publicou sua primeira tira diária, com as aventuras do garoto Franjinha e do seu cãozinho Bidu. As tiras de Mauricio de Sousa espalharam-se pelos jornais de todo o país, levando-o a montar um estúdio que hoje dá vida a mais de 400 personagens.
-            Em 1970, lançou a revista Mônica e, em 1971, recebeu o mais importante prêmio do mundo dos quadrinhos, o troféu Yellow Kid, em Lucca, na Itália. Seguindo o sucesso de Mônica, outros personagens também ganharam suas próprias revistas, que já passaram pelas editoras Abril e Globo e atualmente estão na Panini.
-            Dos quadrinhos, eles foram para o teatro, o cinema, a televisão, a internet, parques temáticos e até para exposições de arte.
-          </p>
-        </div>
-      </div>
-
-</article>
-      
-<article class="row pt-5 ">
-
-        <div class="col-12 col-sm-12 col-lg-6 pt-5 text-center">
- <figure>
-          <img src=" ../HQS/img/a2.jpg" class="img-fluid" alt=" Aqui temos a imagem do Katsuhiro Otomo">
-          <figcaption> Katsuhiro Otomo</figcaption>
-</figure>
-        
-        <div class="col-12 col-sm-12 col-lg-6 pt-5 pb-5 text-start">
-          <div class="cor-fundo">
-            <h2 class="cor-letra">Katsuhiro Otomo</h2>
-            <p>Mundialmente famoso por ser o autor tanto do mangá quanto da animação de Akira, Katsuhiro Otomo nasceu na província de Miyagi, Japão no ano de 1954.
-              A Gun Report, de 1973, foi sua primeira obra e foi um grande sucesso, se tornando um dos mangás mais vendidos na época. Fireball, lançado em 1979, o consagrou em definitivo como autor de estilo mais ‘cult’. No começo dos anos 1980, Otomo começou a publicar sua obra mais famosa. Akira foi iniciado em 1982 nas páginas da Young Magazine e seria finalizado apenas em 1990, depois do lançamento da versão animada, de 1988, nos cinemas.
-              Depois do sucesso avassalador de Akira, Katsuhiro Otomo lançou Domu, obra essa que lhe rendeu o Grande Prêmio de Ficção Científica Japonês em 1983, feito até então inédito no mundo dos mangás.
-            </p>
-          </div>
-        </div>
-</article>
-
-  <footer>
-    <?php include "../rodape.php" ?>
-  </footer>
-  </div>
-
-</main>
+    <?php include "../nav.php"?>
+    <div class="container">
+        <article>
+            <div class="row text-center">
+                <h1>Séries e TV</h1>
+            </div>
+            <section>
+                    <i class="bi bi-square-fill fs-1 d-inline cor-letra"></i>
+                    <h2 class="d-inline cor-letra">Atores</h2>
+            </section>
+            <div class="row">
+                <section class="col-12 col-sm-6">
+                    <div class="text-center cor-fundo p-3">
+                        <i class="bi bi-square-fill fs-1 d-inline cor-letra"></i>
+                        <h3 class="d-inline">Katherine McNamara revela paixão pela comunidade e fãs brasileiros na CCXP22</h3>
+                    </div>
+                    <div class="text-center cor-fundo p-3">
+                        <img src="../img/katherine.png" class="img-fluid" alt="imagem katherine">
+                        <p>Katherine McNamara trouxe simpatia e encantou o público do Palco Thunder by Cinemark Club durante a CCXP22. 
+                            Em uma conversa com Maria Bopp, a artista contou sobre a carreira, a paixão pelo Brasil e o carinho dos fãs das franquias que já atuou.
+                        </p>
+                        <p>A atriz possui um enorme carinho pelo país, ela esteve presente na CCXP14 e foi sua primeira participação em uma convenção. "Adoro o Brasil e os brasileiros, 
+                            o público é tão carinhoso, há tanto amor pela comunidade, é um prazer estar de volta" disse, encantada.
+                        </p>
+                        <p>McNamara é reconhecida pelos fãs por participar de produções de ficção e fantasia como Shadowhunters, Maze Runner e Arrow, e conta que, apesar de ter caído de 
+                            paraquedas no gênero, sentiu uma conexão instantânea. "Sou grata, cresci com esses mundos [de fantasia] e queria vivenciar isso. Sei como é a responsabilidade 
+                            de lidar com esses personagens e me sinto agradecida de poder vivenciá-los."
+                        </p>
+                        <p>Shadowhunters é uma das franquias pelas quais ela tem grande apreço, tanto criou ao lado do ator Dominic Sherwood o podcast Return to the Shadows, focado no 
+                            universo e bastidores da série. Segundo a atriz, a programa aproximou-a dos fãs, e ela “jogou para o universo” o desejo em reviver a série - caso alguma produtora queira.
+                        </p>
+                        <p>Durante o painel, McNamara disse achar incrível como os eventos como CCXP são importantes para os fãs de franquias e atores e artistas. "Eu aprendi como a comunidade de 
+                            convenções é incrível, a gente conta essas histórias porque a gente se empodera e traz um sentimento de pertencimento"
+                        </p>
+                        <a class="text-decoration-none cor-letra" href="../autores.php?#giovannabreve"><p>Giovanna Breve,03/12/2022.</p></a>
+                    </div>
+                </section>
+                <section class="col-12 col-sm-6">
+                    <div class="text-center p-3">
+                        <i class="bi bi-square-fill fs-1 d-inline cor-letra"></i>
+                        <h3 class="d-inline">Seu Barriga emociona CCXP22 em painel com tom de despedida</h3>
+                    </div>
+                    <div class="text-center p-3">
+                        <img src="../img/seubarriga.png" class="img-fluid" alt="imagem seu barriga">
+                        <p> O consagrado ator Edgar Vivar emocionou o público do Palco Thunder by Cinemark Club compartilhando com o público histórias de bastidores que envolvem o amado personagem Seu Barriga. 
+                            O ator mexicano interpretou o dono da vila do Chaves e ficou famoso no Brasil com seu bordão: "pague os 14 meses de aluguel!"
+                        </p>
+                        <p>Perto de se aposentar devido a um problema na perna, Vivar anda com certa dificuldade e deve passar por um procedimento que o colocará em repouso forçado por cerca de seis meses. 
+                            No alto dos seus 73 anos, o ator recebe de bom grado o carinho dos fãs. "Vim muitas vezes para cá, sou praticamente brasileiro. Adoro a comida, a música, o futebol - e gosto de cobrar o aluguel. 
+                            O seriado tem mais de 40 anos, a geração para que Chaves foi produzida já passou", disse ele. "Agora novas gerações desfrutam disso. Muitos nem sabem que o Chaves e o Seu Madruga não estão mais aqui!".
+                        </p>
+                        <p>Relembrando o início da produção, Vivar disse que antes mesmo de saberem se os projetos de Roberto Gomez Bolaños (o Chaves) daria certo, o elenco gravou, em duas semanas, vários episódios de A Turma do Chavez,
+                             Chapolin Colorado e Cidadão Gómez - que não chegou ao Brasil. "Mas Chaves foi um sucesso tremendo", afirmou.
+                        </p>
+                        <a class="text-decoration-none cor-letra" href="../autores.php?#Pedrohenriqueribeiro"><p>Pedro Henrique Ribeiro 04/12/2022</p></a>
+                    </div>
+                </section>
+            </div>
+            <div class="text-end pb-3">
+                <a class="btn btn-primary" href="../Seriesetv.php/serieshome.php" role="button">Voltar</a>
+            </div> 
+        </article>
+    </div> 
+    <?php include "../rodape.php"?>  
 </body>
 </html>
+    
