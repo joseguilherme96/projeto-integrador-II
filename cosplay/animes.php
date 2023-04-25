470f22619e99759ec990adaa152d39cd443d3f57
Giliarde0 <gilbertogiliarde0@gmail.com>
2023-04-25 17:41:36 -0300

    colocando divs

--- cosplay/animes.php
+++ cosplay/animes.php
@@ -11,26 +11,38 @@
   </head>
 <body>
   <?php include "../nav.php"?>
+  <div class="container ">
       <main>
-        <section>
-          <h1 class="cor-letra"> ANIMES </h1>
-          <article> <h2> Boruto </h2>   <a href="https://www.crunchyroll.com/pt-br/series/GR75Q020Y/boruto-naruto-next-generations"><figure><figcaption> <img src=" imgcosplay/boruto.png" alt=" Uzumaki Boruto, filho de Uzumaki Naruto, o Sétimo Hokage, se inscreveu na Academia Ninja para aprender a ser um verdadeiro ninja. Os outros estudantes o ignoram por ser apenas o filho do Hokage mas a paixão e a personalidade do Boruto vai acabar com todos esses preconceitos.Quando uma série de misteriosos eventos começa a se desenrolar, cabe a Boruto e seus novos amigos a investigá-los. Como um tornado, Boruto entra no coração de todos: sua história está prestes a começar!"> </a>  </figcaption></figure></article> 
 
+        <section class="row">
+        <div class="col-12 cor-letra">
+          <h1 class="cor-letra"> ANIMES </h1> 
+        </div>
+
+        <div class="row">
+
+          <article> <h2> Boruto </h2>   <a href="https://www.crunchyroll.com/pt-br/series/GR75Q020Y/boruto-naruto-next-generations"><figure><figcaption> <img src=" imgcosplay/boruto.png" alt=" Uzumaki Boruto, filho de Uzumaki Naruto, o Sétimo Hokage, se inscreveu na Academia Ninja para aprender a ser um verdadeiro ninja. Os outros estudantes o ignoram por ser apenas o filho do Hokage mas a paixão e a personalidade do Boruto vai acabar com todos esses preconceitos.Quando uma série de misteriosos eventos começa a se desenrolar, cabe a Boruto e seus novos amigos a investigá-los. Como um tornado, Boruto entra no coração de todos: sua história está prestes a começar!"> </a>  </figcaption></figure></article> 
+        
           <article> <h2> My Hero Academy </h2>  <a href="https://www.crunchyroll.com/pt-br/series/G6NQ5DWZ6/my-hero-academia"><figure><figcaption><img src=" imgcosplay/myhero.png" alt="Por toda a sua vida, Izuku sonhou ser um heroi — um objetivo ambicioso para qualquer um, mas especialmente desafiador para um garoto sem superpoderes. Isso mesmo: em um mundo onde 80% da população tem algum tipo de Dom especial, Izuku teve a má sorte de nascer completamente normal. Mas isso não vai impedi-lo de se matricular em uma das academias de herois mais prestigiosas do mundo."></a>  </figcaption></figure></article> 
 
           <article> <h2> Dragon Ball Super </h2> <a href="https://www.crunchyroll.com/pt-br/series/GR19V7816/dragon-ball-super"><figure><figcaption><img src=" imgcosplay/dragonballsuper.png" alt="18 anos depois, temos uma nova saga de Dragon Ball das mãos de seu criador, Akira Toriyama. Após a derrota de Majin Buu, Goku arranja um novo emprego como... fazendeiro de nabos? Agora que a Terra está em paz, nosso heróis levam vidas mundanas, mas não por muito tempo.Longe dali, Beerus, o poderoso Deus da Destruição, recebe uma profecia de que encontrará seu fim nas mãos de um ser ainda mais poderoso, e sua busca pelo Deus dos Saiya-jins o traz para a Terra. Será que Goku e seus amigos conseguirão derrotar seu mais poderoso inimigo até então?"></a> <figcaption></figure></article> 
 
           <article> <h2> Hunter X Hunter </h2> <a href="https://www.crunchyroll.com/pt-br/series/GY3VKX1MR/hunter-x-hunter"><figure><figcaption><img src="imgcosplay/jujutsukaisen.png" alt="Monstros amedrontadores, criaturas exóticas, riquezas vastas, tesouros misteriosos, terras vis e terras inexploradas... Gon parte em uma aventura para se tornar um caçador profissional que arrisca a própria vida em busca do desconhecido. Pelo caminho, ele conhece outros participantes da Prova dos Caçadores: Kurapika, Leorio e Killua. Será Gon capaz de vencer os grandes desafios da Prova de Caçadores e se tornar o melhor do mundo? Esta jornada selvagem e épica está prestes a começar!!"> </a> </figcaption></figure></article> 
+      
+        </div>
+        
+        <div class="row">
 
           <article> <h2> Demons Slayer </h2> <a href="https://www.crunchyroll.com/pt-br/series/GY5P48XEY/demon-slayer-kimetsu-no-yaib"> <img src=" imgcosplay/demonslayer.png" alt="Japão, era Taisho. Tanjiro, um bondoso jovem que ganha a vida vendendo carvão, descobre que sua família foi massacrada por um demônio. E pra piorar, Nezuko, sua irmã mais nova e única sobrevivente, também foi transformada num demônio. Arrasado com esta sombria realidade, Tanjiro decide se tornar um matador de demônios para fazer sua irmã voltar a ser humana, e para matar o demônio que matou sua família. Um triste conto sobre dois irmãos, onde os destinos dos humanos e dos demônios se entrelaçam, começa agora."></a>  </figcaption></figure></article> 
-
+        
           <article> <h2> Jujutsu Kaisen </h2> <a href="https://www.crunchyroll.com/pt-br/series/GRDV0019R/jujutsu-kaisen"> <figure><figcaption><img src=" imgcosplay/jujutsukaisen.png" alt="Sofrimento, arrependimento, vergonha: os sentimentos negativos dos humanos tornam-se Maldições, causando terríveis acidentes que podem levar até mesmo à morte. E pra piorar, Maldições só podem ser exorcizadas por outras Maldições. Certo dia, para salvar amigos que estavam sendo atacados por Maldições, Yuji Itadori engole o dedo do Ryomen-Sukuna, absorvendo sua Maldição. Ele então decide se matricular no Colégio Técnico de Feitiçaria de Tóquio, uma organização que combate as Maldições... e assim começa a heróica lenda do garoto que tornou-se uma Maldição para exorcizar uma Maldição."> </a> </figcaption></figure></article> 
 
           <article> <h2> One Punch man </h2> <a href="https://www.crunchyroll.com/pt-br/series/G63K98PZ6/one-punch-man"> <figure><figcaption><img src=" imgcosplay/onepunchman.png" alt="Saitama é um cara que virou herói por diversão. Depois de realizar um *treino especial* por três anos, ele ficou tão poderoso que consegue derrotar qualquer oponente com um único soco. Agora, ao lado de Genos, seu fiel discípulo ciborgue, Saitama está pronto para encarar seus deveres como herói profissional da Associação de Heróis. Contudo, monstros têm aparecido com frequência cada vez maior, o que confirma as profecias da Grande Vidente Madame Shibabawa sobre o fim da Terra. E em meio a toda essa crise, surge Garou, o *caçador* de herois."> </a>  </figcaption></figure></article> 
 
           <article> <h2> Tokyo Revengers </h2> <a href="https://www.crunchyroll.com/pt-br/series/G3KHEVMN1/tokyo-revengers"> <figure><figcaption><img src=" imgcosplay/tokyorevengers.png" alt="Takemichi Hanagaki é um desempregado que sobrevive de bicos e está na fossa. Ele descobriu que Hinata Tachibana, sua primeira e última namorada, com quem namorou no fundamental, foi morta pela impiedosa Gangue Manji de Tóquio. No dia seguinte à notícia, ele está na beira da plataforma do trem e é empurrado pela multidão. Ele fecha os olhos se preparando para morrer, mas ao abrir, ele voltou no tempo para quando tinha 12 anos de idade. Agora que ele está na melhor época de sua vida, Takemichi decide se vingar de sua vida, salvando sua namorada e parando de fugir de si mesmo."> </a>  </figcaption></figure></article> 
+        
+        </div>
 
-          <article> <h2> Bleach </h2> <a href="https://www.crunchyroll.com/pt-br/series/GG5H5XK80/bleach-international-dubs"> <figure><figcaption><img src=" imgcosplay/bleach.png" alt="O jovem Ichigo é surpreendido com indesejadas habilidades de ceifeiro de almas e quer devolver os poderes. Mas deve executar algumas tarefas antes."> </figcaption></figure> </article>
         <section>
       </main>
   <?php include "../rodape.php"?>
--- cosplay/cosplay.php
+++ cosplay/cosplay.php
@@ -9,8 +9,16 @@
 </head>
 <body>
   <?php include "../nav.php" ?>
+  <div class="container ">
     <main>
+      
+    <div class="row">
+    <div class="col-12 cor-letra">
       <h1> COSPLAY </h1>
+    </div>
+
+    <div class="row">
+      
         <article>
         <a href="https://www.instagram.com/p/ClEcb9ojfda/?utm_source=ig_embed&ig_rid=ec94feb5-a4f1-49f9-bc25-1625c4ae9ef5"><figure> <figcaption> <img src=" imgcosplay/kodia.jpg " alt="Ahsoka Tano - Star Wars Incrivel Cosplay do personagem Ahsoka Tano feita por uma fã da grande série Star Wars surpreendeu nas redes sociais, a fã descreve *Esse cosplay foi um verdadeiro desafio pra mim, principalmente a maquiagem, e aprendi muito fazendo.Mal posso esperar para usá-la na Star Wars Celebration em Londres no ano que vem."> </a> </figcaption> </figure>
         <h2> Ahsoka Tano - Star Wars </h2>
