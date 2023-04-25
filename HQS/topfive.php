d9cad80b84f1345e0b00736f48ac3823bde3c866
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2023-04-25 17:34:02 -0300

    update yasmin

--- HQS/topfive.php
+++ HQS/topfive.php
@@ -22,7 +22,7 @@
 
   <div class="row">
     <div class="col-12 cor-letra">
-      <a href="../pg.html" class="text-decoration-none cor-letra">Home</a> / <a href="autores.html" class="text-decoration-none cor-letra">Autores</a>/ <a href="criticas.html" class="text-decoration-none cor-letra">Criticas</a> / <a href="materia.html" class="text-decoration-none cor-letra">Materias
+      <a href="../pg.php" class="text-decoration-none cor-letra">Home</a> / <a href="autores.php" class="text-decoration-none cor-letra">Autores</a>/ <a href="criticas.php" class="text-decoration-none cor-letra">Criticas</a> / <a href="materia.php" class="text-decoration-none cor-letra">Materias
       / <a href="topfive.html" class="text-decoration-none cor-letra">Top Five</a>
     
     </div>
