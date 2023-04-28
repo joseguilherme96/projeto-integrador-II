d08415cb909c8f18942e3f7133b1de01a1409e7c
Giliarde0 <gilbertogiliarde0@gmail.com>
2023-04-28 01:46:40 -0300

    deafea

--- cosplay/cosplay.php
+++ cosplay/cosplay.php
@@ -9,34 +9,42 @@
 </head>
 <body>
   <?php include "../nav.php" ?>
-    <main class="container justify-content-center">
-      <section class="row text-center col-12 pt-5">
+    <main class="container text-center">
+      <section class="row justify-content-center pt-5">
+
         <h1 class="cor-letra"> COSPLAY </h1>
-          <article class="col-3 col-sm-3 col-lg-3 col-xl-3 pt-5">
+        
+        <div class="row col-6 col-sm-6 col-lg-6 col-xl-4 pt-5"> 
+          <article > 
             <a href="https://www.instagram.com/p/ClEcb9ojfda/?utm_source=ig_embed&ig_rid=ec94feb5-a4f1-49f9-bc25-1625c4ae9ef5"><figure> <figcaption> <img src=" imgcosplay/kodia.jpg " alt="Ahsoka Tano - Star Wars Incrivel Cosplay do personagem Ahsoka Tano feita por uma fã da grande série Star Wars surpreendeu nas redes sociais, a fã descreve *Esse cosplay foi um verdadeiro desafio pra mim, principalmente a maquiagem, e aprendi muito fazendo.Mal posso esperar para usá-la na Star Wars Celebration em Londres no ano que vem."> </a> </figcaption> </figure>
             <h2 class="cor-letra"> Ahsoka Tano - Star Wars </h2>
-            <p>Incrivel Cosplay do personagem "Ahsoka Tano" feita por uma fã da grande série Star Wars surpreendeu nas redes sociais, a fã descreve "Esse cosplay foi um verdadeiro desafio pra mim, principalmente a maquiagem, e aprendi muito fazendo.Mal posso esperar para usá-la na Star Wars Celebration em Londres no ano que vem.</p>
-
+            <p>Incrivel Cosplay do personagem "Ahsoka Tano" feita por uma fã da grande série Star Wars surpreendeu nas redes sociais</p>
+        </div>
+        <div class="col-6 col-sm-6 col-lg-6 col-xl-4 pt-5"> 
             <a href="https://www.instagram.com/jennifercosplays/?utm_source=ig_embed&ig_rid=fc50bd0c-6612-4040-a0c9-3b7372100f2c"><figure> <figcaption> <img src=" imgcosplay/arlequina.jpg" alt="Deadpool e Arlequina Deadpool e Arlequina juntos em incrivel cosplay feito por fã, quem conhece sabe que os dois personagem são de casas de super-herois diferentes, então essa amizade é bem improvavel acontecer um dia, em algum filme, mas os fãs quebram barreiras. "> </a></figcaption> </figure>
             <h2 class="cor-letra">Deadpool e Arlequina </h2>
             <p>Deadpool e Arlequina juntos em incrivel cosplay feito por fã, quem conhece sabe que os dois personagem são de casas de super-herois diferentes, então essa amizade é bem improvavel acontecer um dia, em algum filme, mas os fãs quebram barreiras. </p>
-            
+        </div>
+        <div class="col-6 col-sm-6 col-lg-6 col-xl-4 pt-5">
             <a href="https://animenew.com.br/god-of-war-ragnarok-cosplay-impecavel-deixa-fas-surpresos/"><figure> <figcaption><img src="imgcosplay/gowcosplay.jpg" alt="God of War Ragnarök Cosplay surpreende fãs da grande franquia, com o recém lançamendo do jogo God of War Ragnarök, que está sendo um sucesso absurdo, aclamada em todo o mundo, alguns comentarios positivos surgiram como este *A equipe envolvida na construção desses cosplays é simplesmente incrível*"> </a></figcaption> </figure>
             <h2 class="cor-letra">God of War Ragnarök</h2>
             <p>Cosplay surpreende fãs da grande franquia, com o recém lançamendo do jogo God of War Ragnarök, que está sendo um sucesso absurdo, aclamada em todo o mundo, alguns comentarios positivos surgiram como este "A equipe envolvida na construção desses cosplays é simplesmente incrível". </p>
-
+        </div>
+        <div class="row justify-content-center col-6 col-sm-6 col-lg-4 col-xl-4 ">
             <a href="https://twitter.com/omelete/status/1144281826183327744"> <figure> <figcaption> <img src=" imgcosplay /capitaoamerica.jpeg" alt="Capitão America Incrivel cosplay de capitão america surpreende na CCXP, diretamente de guerra infinita, um dos herois mais queridos do mundo dos cinemas."> </a></figcaption> </figure>
             <h2 class="cor-letra">Capitão America </h2>
             <p>Incrivel cosplay de capitão america surpreende na CCXP, diretamente de guerra infinita, um dos herois mais queridos do mundo dos cinemas.</p>
-
+        </div>
+        <div class="col-6 col-sm-6 col-lg-4 col-xl-4  ">
             <a href="https://www.instagram.com/jennifercosplays/?utm_source=ig_embed&ig_rid=fc50bd0c-6612-4040-a0c9-3b7372100f2c"> <figure> <figcaption><img src=" imgcosplay/gueen.jpg" alt="Gueen Fã faz cosplay de Gueen, uma das personagens mais amada da serie de filmes do Homem Aranha, além desses a fã ja fez diversos outros personagens."> </a></figcaption> </figure>
             <h2 class="cor-letra">Gueen</h2>
             <p> Fã faz cosplay de Gueen, uma das personagens mais amada da serie de filmes do Homem Aranha, além desses a fã ja fez diversos outros personagens.</p>
-
+        </div>
+        <div class="col-6 col-sm-6 col-lg-4 col-xl-4 ">
             <a href="https://www.instagram.com/jennifercosplays/?utm_source=ig_embed&ig_rid=fc50bd0c-6612-4040-a0c9-3b7372100f2c"> <figure> <figcaption> <img src=" imgcosplay/sylvie.jpg" alt="Sylvie Quem ai conhece a grande *vilã* da serie Loki? Sylvie, uma personagem bastante imponente e que faz parte de um dos grandes estopims da marvel."></a></figcaption> </figure>
             <h2 class="cor-letra">Sylvie</h2>
             <p> Quem ai conhece a grande "vilã" da serie Loki? Sylvie, uma personagem bastante imponente e que faz parte de um dos grandes estopims da marvel. </p>
-
+        </div>
           </article>
       </section>
     </main>
