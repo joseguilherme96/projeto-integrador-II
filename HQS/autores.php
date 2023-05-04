f81acb13ec0c2b6cdd866ff269f40ba81340ed2d
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2023-05-04 16:51:34 -0300

    Update autores.php

--- HQS/autores.php
+++ HQS/autores.php
@@ -3,10 +3,9 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>HQS</title>
+  <title>Autores</title>
   <link href="../css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-  <link rel="stylesheet" href="../node_modules/bootstrap-icons/font/bootstrap-icons.css">
   <link rel="stylesheet" href="../css/tema.css">
 </head>
 
@@ -22,10 +21,9 @@
    
 
   <div class="container">
-    <div class="row">
-      <div class="col-12 cor-letra">
+    <div class="col-12 cor-letra ROW">
+   
         <a href="../pg.php" class="text-decoration-none cor-letra">Home</a> / <a href="autores.php" class="text-decoration-none cor-letra">Autores</a>
-      </div>
     </div>
     <h1 class="col-12 cor-letra text-center">Autores</h1>
   
@@ -78,19 +76,17 @@
 
  
 
-<article class="row pt-5 ">
+<article class="row pt-5 " >
 
-      <div class="col-12 col-sm-12 col-lg-6 text-center pt-5 ">
-
-        <figure>
+  
+        <figure class="col-12 col-sm-12 col-lg-6 text-center pt-5 ">
         <img src=" ../HQS/img/a1.jpg" class="img-fluid" alt="Aqui temos a imagem do Mauricio de Sousa">
         <figcaption> Mauricio de Sousa </figcaption>
         </figure>
-      </div>
+     
 
 
-      <div class="col-12 col-sm-12 col-lg-6 pt-5 pb-5 text-start ">
-        <div class="cor-fundo">
+      <div class="col-12 col-sm-12 col-lg-6 pt-5 pb-5 text-start cor-fundo">
           <h2 class="cor-letra">Mauricio de Sousa</h2>
           <p>Mauricio de Sousa é desenhista, empresário, o mais premiado autor brasileiro de quadrinhos e membro da Academia Paulista de Letras. Nasceu em 27 de outubro de 1935, numa família de poetas e contadores de histórias em Santa Isabel, no interior de São Paulo. Ainda criança, mudou-se para Mogi das Cruzes, onde descobriu sua paixão pelo desenho e começou a criar os primeiros personagens. Com 19 anos, foi para São Paulo tentar trabalhar como ilustrador na Folha da Manhã (hoje Folha de S.Paulo). No entanto, conseguiu apenas uma vaga de repórter policial.
             Em 1959, publicou sua primeira tira diária, com as aventuras do garoto Franjinha e do seu cãozinho Bidu. As tiras de Mauricio de Sousa espalharam-se pelos jornais de todo o país, levando-o a montar um estúdio que hoje dá vida a mais de 400 personagens.
@@ -98,28 +94,26 @@
             Dos quadrinhos, eles foram para o teatro, o cinema, a televisão, a internet, parques temáticos e até para exposições de arte.
           </p>
         </div>
-      </div>
+      
 
 </article>
       
 <article class="row pt-5 ">
 
-        <div class="col-12 col-sm-12 col-lg-6 pt-5 text-center">
- <figure>
+       
+ <figure class="col-12 col-sm-12 col-lg-6 pt-5 text-center">
           <img src=" ../HQS/img/a2.jpg" class="img-fluid" alt=" Aqui temos a imagem do Katsuhiro Otomo">
           <figcaption> Katsuhiro Otomo</figcaption>
 </figure>
-        </div>
-        
-        <div class="col-12 col-sm-12 col-lg-6 pt-5 pb-5 text-start">
-          <div class="cor-fundo">
+       
+        <div class="col-12 col-sm-12 col-lg-6 pt-5 pb-5 text-start cor-fundo">
             <h2 class="cor-letra">Katsuhiro Otomo</h2>
             <p>Mundialmente famoso por ser o autor tanto do mangá quanto da animação de Akira, Katsuhiro Otomo nasceu na província de Miyagi, Japão no ano de 1954.
               A Gun Report, de 1973, foi sua primeira obra e foi um grande sucesso, se tornando um dos mangás mais vendidos na época. Fireball, lançado em 1979, o consagrou em definitivo como autor de estilo mais ‘cult’. No começo dos anos 1980, Otomo começou a publicar sua obra mais famosa. Akira foi iniciado em 1982 nas páginas da Young Magazine e seria finalizado apenas em 1990, depois do lançamento da versão animada, de 1988, nos cinemas.
               Depois do sucesso avassalador de Akira, Katsuhiro Otomo lançou Domu, obra essa que lhe rendeu o Grande Prêmio de Ficção Científica Japonês em 1983, feito até então inédito no mundo dos mangás.
             </p>
           </div>
-        </div>
+        
 </article>
 
   <footer>
