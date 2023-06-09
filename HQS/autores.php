527d65fab3daeacb413b35af56e190badeed42aa
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2023-06-08 22:07:46 -0300

    Update autores.php

--- HQS/autores.php
+++ HQS/autores.php
@@ -1,66 +1,30 @@
 <?php $title = "Autores";?>
 <?php include "../header.php"?>
-    <div class="container">
-<body>
-<main>
 
-<section>
+ <body>
+  <main>
+
+   <div class="container">
+    <section>
   
-    <h1 class="col-12 cor-letra text-center">Autores</h1>
-
-  <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
-
-       <div class="carousel-inner">
-         <div class="carousel-item active">
-          <figure>
-         <img src=" ../HQS/img/a22.jpg" class="d-block w-100" alt="Quadrinhos">
-           <figcaption> quadrinhos</figcaption>
-            </figure>
-               <div class="carousel-caption d-none d-md-block">
-                 </div>
-                   </div>
-                      <div class="carousel-item">
-                          <figure>
-                               <img src=" ../HQS/img/a23.jpg" class="d-block w-100" alt="Quadrinhos">
-                                  <figcaption> quadrinhos</figcaption>
-                                      </figure>
-                                         <div class="carousel-caption d-none d-md-block">
-                                            </div>
-                                               </div>
-                                                  <div class="carousel-item">
-                                               <figure>
-                                              <img src=" ../HQS/img/a24.jpg" class="d-block w-100" alt="Quadrinhos">
-                                          <figcaption> quadrinhos</figcaption>
-                                       </figure>
-                                     <div class="carousel-caption d-none d-md-block">
-                                  </div>
-                               </div>
-                             </div>
-                           <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Prev</span>
-                  </button>
-               <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-           <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
-
-
-<article class="row pt-5 " >
-
+     <h1 class="col-12 cor-letra text-center">Autores</h1>
   
-        <figure class="col-12 col-sm-12 col-lg-6 text-center pt-5 ">
-        <img src=" ../HQS/img/a1.jpg" class="img-fluid" alt="Aqui temos a imagem do Mauricio de Sousa">
-        <figcaption> Mauricio de Sousa </figcaption>
+        <figure class="col-12 col-sm-12 col-lg-12 text-center pt-5 ">
+          <img src=" ../HQS/img/a22.jpg" class="img-fluid" alt="Aqui temos a imagem do Mauricio de Sousa">
+            <figcaption> Mauricio de Sousa </figcaption>
         </figure>
+     
 
-      <div class="col-12 col-sm-12 col-lg-6 pt-5 pb-5 text-start cor-fundo">
+
+      <article class="row pt-5 " >
+
+  
+             <figure class="col-12 col-sm-12 col-lg-6 text-center pt-5 ">
+               <img src=" ../HQS/img/a1.jpg" class="img-fluid" alt="Aqui temos a imagem do Mauricio de Sousa">
+                 <figcaption> Mauricio de Sousa </figcaption>
+             </figure>
+
+       <div class="col-12 col-sm-12 col-lg-6 pt-5 pb-5 text-start cor-fundo">
           <h2 class="cor-letra">Mauricio de Sousa</h2>
           <p>Mauricio de Sousa é desenhista, empresário, o mais premiado autor brasileiro de quadrinhos e membro da Academia Paulista de Letras. Nasceu em 27 de outubro de 1935, numa família de poetas e contadores de histórias em Santa Isabel, no interior de São Paulo. Ainda criança, mudou-se para Mogi das Cruzes, onde descobriu sua paixão pelo desenho e começou a criar os primeiros personagens. Com 19 anos, foi para São Paulo tentar trabalhar como ilustrador na Folha da Manhã (hoje Folha de S.Paulo). No entanto, conseguiu apenas uma vaga de repórter policial.
             Em 1959, publicou sua primeira tira diária, com as aventuras do garoto Franjinha e do seu cãozinho Bidu. As tiras de Mauricio de Sousa espalharam-se pelos jornais de todo o país, levando-o a montar um estúdio que hoje dá vida a mais de 400 personagens.
@@ -69,16 +33,16 @@
           </p>
         </div> 
 
-</article>
+      </article>
       
-<article class="row pt-5 ">
+       <article class="row pt-5 ">
     
- <figure class="col-12 col-sm-12 col-lg-6 pt-5 text-center">
+         <figure class="col-12 col-sm-12 col-lg-6 pt-5 text-center">
           <img src=" ../HQS/img/a2.jpg" class="img-fluid" alt=" Aqui temos a imagem do Katsuhiro Otomo">
           <figcaption> Katsuhiro Otomo</figcaption>
-</figure>
+         </figure>
        
-        <div class="col-12 col-sm-12 col-lg-6 pt-5 pb-5 text-start cor-fundo">
+          <div class="col-12 col-sm-12 col-lg-6 pt-5 pb-5 text-start cor-fundo">
             <h2 class="cor-letra">Katsuhiro Otomo</h2>
             <p>Mundialmente famoso por ser o autor tanto do mangá quanto da animação de Akira, Katsuhiro Otomo nasceu na província de Miyagi, Japão no ano de 1954.
               A Gun Report, de 1973, foi sua primeira obra e foi um grande sucesso, se tornando um dos mangás mais vendidos na época. Fireball, lançado em 1979, o consagrou em definitivo como autor de estilo mais ‘cult’. No começo dos anos 1980, Otomo começou a publicar sua obra mais famosa. Akira foi iniciado em 1982 nas páginas da Young Magazine e seria finalizado apenas em 1990, depois do lançamento da versão animada, de 1988, nos cinemas.
@@ -86,14 +50,14 @@
             </p>
           </div>
         
-</article>
+       </article>
 
-  <footer>
-    <?php include "../rodape.php" ?>
-  </footer>
+           <footer>
+              <?php include "../rodape.php" ?>
+           </footer>
 
-  </div>
-</section>
-</main>
+    </section>
+   </div>
+  </main>
 </body>
 </html>
