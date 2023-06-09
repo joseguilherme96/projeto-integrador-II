1ca884f342801cacd552780baa45482ff904f9a3
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2023-06-08 22:26:28 -0300

    validada e identada

--- HQS/pg.php
+++ HQS/pg.php
@@ -1,13 +1,13 @@
 <?php $title = "HQS";?>
 <?php include "../header.php"?>
-    <div class="container">
 <body>
   <main>
+   <div class="container">
       <section>
               <h1 class="text-center cor-letra">HQS</h1>
-      <article >
+       <article >
                         <figure class="text-center">
-                            <a href="materia.php"><img src="../HQS/img/m1.jpg" img-fluid src="../HQS/img/c1.jpg" class="img-fluid" alt=" Aqui temos a imagem do CCXP22"></a>
+                            <a href="materia.php"><img src="../HQS/img/m1.jpg" class="img-fluid" alt=" Aqui temos a imagem do CCXP22"></a>
                             <figcaption>CCXP22</figcaption>
                         </figure>
                 <div class="row text-start cor-fundo">
@@ -15,9 +15,9 @@
                   <a class="text-decoration-none cor-letra" href="materia.php"><h2 class="d-inline">A Comic Con Experience (CCXP) 2022 começou na quinta-feira (1) e ocorre até o próximo domingo (4) na São Paulo Expo.</h2></a>
                   <a class="text-dark text-decoration-none" href="materia.php"><p>Muitos filmes e séries aparecerão em estandes na CCXP 2022...</p></a>  
                 </div>
-      </article>
+       </article>
       
-      <article class="row">
+       <article class="row">
                         <figure class="col-12 col-sm-4 pt-5 mt-sm-3 pb-3">
                             <a href="topfive.php"><img src="../HQS/img/t1.jpg" class="img-fluid" alt="Aqui temos a imagem do livro dias gigantes"></a> 
                             <figcaption>Dias Gigantes</figcaption>
@@ -32,14 +32,15 @@
                         <a href="topfive.php"><img src="../HQS/img/t3.jpg" class="img-fluid" alt="Aqui temos a imagem do livro Doce Jazz"></a> 
                         <figcaption>Doce Jazz</figcaption>
                     </figure>
-                      
-      <article class="col-12 col-sm-12 pt-5 mt-sm-3 pb-3" >
+        </article>
+                  
+        <article class="col-12 col-sm-12 pt-5 mt-sm-3 pb-3" >
                             <a class="text-decoration-none cor-letra text-start cor-fundo d-block" href="topfive.php"  ><i class="bi bi-square-fill fs-1 d-inline cor-letra text-start cor-fundo"></i><h2 class="d-inline">Formosa Márcia vence o Prêmio Jabuti na categoria História em Quadrinhos</h2></a>
                             <a class="text-dark text-decoration-none pb-3 text-start cor-fundo " href="topfive.php"><p class="cor-fundo">Foi divulgado na data de ontem o vencedor da 64ª edição do Prêmio Jabuti na categoria História em Quadrinhos. O prêmio foi para Escuta, Formosa Márcia, de Marcello Quintanilha (Editora Veneta).</p></a>
 
-      </article>
+        </article>
                 
-      <article class="row"> 
+       <article class="row"> 
                         <figure class="col-12 col-sm-4 pt-5 mt-sm-3">
                             <a href="critica.php"><img src="../HQS/img/c3.jpg" class="img-fluid" alt=" Aqui temos a imagem do Jeff Smith: Bone e a longa jornada épica do “faça você mesmo"></a> 
                             <figcaption>Jeff Smith: Bone e a longa jornada épica do “faça você mesmo”</figcaption>
@@ -60,7 +61,7 @@
                             <a class="text-decoration-none cor-letra" href="ultimasnoticias.php"><h2 class="d-inline">Desenhão: batalha de desenhos ao vivo premiará artista com um iPad Pro</h2></a>
                             <a class="text-dark text-decoration-none" href="ultimasnoticias.php"><p>No próximo sábado, 26 de novembro, a partir das 19h, acontece o Desenhão, uma festa que vai reunir artistas, fãs dos quadrinhos nacionais e amantes da cultura geek no geral numa oportunidade de juntar as pessoas para celebrar, debater assuntos e fazer arte. </p></a>
                         </div>
-        </article>
+       </article>
                
         <article class="row">
                     
@@ -74,14 +75,11 @@
                             <a class="text-decoration-none cor-letra" href="ultimasnoticias.php"><h2 class="d-inline">História em quadrinhos sobre Paulo Freire em campanha no Catarse</h2></a>
                 
                             <a class="text-dark text-decoration-none" href="ultimasnoticias.php"><p>A obra busca apresentar o educador a uma nova geração, mas também permitir um reencontro àqueles que conhecem sua história, com quatro narrativas criadas por Rogério Faria (Marighella #LIVRE), Ricardo Sousa (ZéMurai), Jefferson Costa (Jeremias - Pele) e Ren Nolasco (Só Ana), com 20 páginas.</p></a>
-                        </div>
-                      
+                      </div>          
         </article>
-
- <footer>  <?php include "../rodape.php"?>  </footer>
-
-              </section>
-              </div>
+         <footer>  <?php include "../rodape.php"?>  </footer>
+      </section>
+   </div>
   </main>
 </body>
 </html>
