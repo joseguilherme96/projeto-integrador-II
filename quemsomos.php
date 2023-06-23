8b6709d46e45f35188eac7a692800ab275891441
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2023-06-23 19:14:43 -0300

    ajuste

--- quemsomos.php
+++ quemsomos.php
@@ -1,15 +1,7 @@
 <!doctype html>
-<html lang="en">
-  <head>
-    <title>HQS</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="css/tema.css">
-  </head>
+<?php $title = "Quem somos";?>
+<?php include "../header.php"?>
 
-<?php include "nav.php"?>
   <div class="container">
     <section class="row pt-5" id="joseguilherme"> 
       <section class="text-center cor-fundo">
