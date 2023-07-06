7b1b273ed3fbcdcdff2839a0873b26ebfd11b79b
joseguilherme96 <jose.guilherme96@outlook.com.br>
2023-07-06 11:55:08 -0300

    Atualização de links

--- footer.php
+++ footer.php
@@ -1,38 +1,38 @@
     <footer class="row  col-12 p-0 m-0 text-light">
         <div class="col-sm-3 col-lg-1 d-none d-sm-inline">
             <ul class="cinemaFooter">
-                <li><a href="/projeto-integrador-acessivel/cinema/indexcinema.php" class="text-decoration-none text-light">Cinema</a></li>
-                <li><a href="/projeto-integrador-acessivel/cinema/filmes.php" class="text-decoration-none text-light">Filmes</a></li>
-                <li><a href="/projeto-integrador-acessivel/cinema/streaming.php" class="text-decoration-none text-light">Streaming</a></li>
-                <li><a href="/projeto-integrador-acessivel/cinema/literatura.php" class="text-decoration-none text-light">Literatura</a></li>
-                <li><a href="/projeto-integrador-acessivel/cinema/entrevistas.php" class="text-decoration-none text-light">Entrevistas</a></li>
+                <li><a href="/projeto-integrador-II/cinema/indexcinema.php" class="text-decoration-none text-light">Cinema</a></li>
+                <li><a href="/projeto-integrador-II/cinema/filmes.php" class="text-decoration-none text-light">Filmes</a></li>
+                <li><a href="/projeto-integrador-II/cinema/streaming.php" class="text-decoration-none text-light">Streaming</a></li>
+                <li><a href="/projeto-integrador-II/cinema/literatura.php" class="text-decoration-none text-light">Literatura</a></li>
+                <li><a href="/projeto-integrador-II/cinema/entrevistas.php" class="text-decoration-none text-light">Entrevistas</a></li>
             </ul>
         </div>
         <div class="col-sm-3 col-lg-1 d-none d-sm-inline">
             <ul class="hqsFooter">
-                <li><a href="/projeto-integrador-acessivel/HQS/indexhqs.php" class="text-decoration-none text-light">HQS</a></li>
-                <li><a href="/projeto-integrador-acessivel/HQS/noticias.php" class="text-decoration-none text-light">Matérias</a></li>
-                <li><a href="/projeto-integrador-acessivel/HQS/livros.php" class="text-decoration-none text-light">Top Five</a></li>
-                <li><a href="/projeto-integrador-acessivel/HQS/melhorescriticas.php" class="text-decoration-none text-light">Melhores Criticas</a></li>
-                <li><a href="/projeto-integrador-acessivel/HQS/melhoresautores.php" class="text-decoration-none text-light">Melhores Autores</a></li>
+                <li><a href="/projeto-integrador-II/HQS/indexhqs.php" class="text-decoration-none text-light">HQS</a></li>
+                <li><a href="/projeto-integrador-II/HQS/noticias.php" class="text-decoration-none text-light">Matérias</a></li>
+                <li><a href="/projeto-integrador-II/HQS/livros.php" class="text-decoration-none text-light">Top Five</a></li>
+                <li><a href="/projeto-integrador-II/HQS/melhorescriticas.php" class="text-decoration-none text-light">Melhores Criticas</a></li>
+                <li><a href="/projeto-integrador-II/HQS/melhoresautores.php" class="text-decoration-none text-light">Melhores Autores</a></li>
         </div>
         <div class="col-sm-3 col-lg-1 d-none d-sm-inline">
             <ul class="cosplayFooter">
-                <li><a href="/projeto-integrador-acessivel/cosplay/cosplay.php" class="text-decoration-none text-light">Cosplay</a></li>
-                <li><a href="/projeto-integrador-acessivel/cosplay/animes.php" class="text-decoration-none text-light">Animes</a></li>
-                <li><a href="/projeto-integrador-acessivel/cosplay/games.php" class="text-decoration-none text-light">Game</a></li>
-                <li><a href="/projeto-integrador-acessivel/cosplay/noticias.php" class="text-decoration-none text-light">Noticias</a></li>
-                <li><a href="/projeto-integrador-acessivel/cosplay/eventos.php" class="text-decoration-none text-light">Eventos</a></li>
+                <li><a href="/projeto-integrador-II/cosplay/cosplay.php" class="text-decoration-none text-light">Cosplay</a></li>
+                <li><a href="/projeto-integrador-II/cosplay/animes.php" class="text-decoration-none text-light">Animes</a></li>
+                <li><a href="/projeto-integrador-II/cosplay/games.php" class="text-decoration-none text-light">Game</a></li>
+                <li><a href="/projeto-integrador-II/cosplay/noticias.php" class="text-decoration-none text-light">Noticias</a></li>
+                <li><a href="/projeto-integrador-II/cosplay/eventos.php" class="text-decoration-none text-light">Eventos</a></li>
             </ul>
         </div>
         <div class="col-sm-3  col-lg-1 d-none d-sm-inline">
             <ul class="seriestvFooter">
-                <li><a href="/projeto-integrador-acessivel/seriesetv/home.php" class=" dropdown-item text-decoration-none text-light">Serie/Tv</a></li>
-                <li><a href="/projeto-integrador-acessivel/seriesetv/resenhas.php" class=" dropdown-item text-decoration-none text-light">Resenhas</a></li>
-                <li><a href="/projeto-integrador-acessivel/seriesetv/lancamentos.php" class="dropdown-item text-decoration-none text-light">Lançamentos</a></li>
-                <li><a href="/projeto-integrador-acessivel/seriesetv/criticas.php" class="dropdown-item text-decoration-none text-light">Criticas</a></li>
-                <li><a href="/projeto-integrador-acessivel/seriesetv/noticias.php" class="dropdown-item text-decoration-none text-light">Notícias</a></li>
-                <li><a href="/projeto-integrador-acessivel/seriesetv/atores.php" class="dropdown-item text-decoration-none text-light">Atores</a></li>
+                <li><a href="/projeto-integrador-II/seriesetv/home.php" class=" dropdown-item text-decoration-none text-light">Serie/Tv</a></li>
+                <li><a href="/projeto-integrador-II/seriesetv/resenhas.php" class=" dropdown-item text-decoration-none text-light">Resenhas</a></li>
+                <li><a href="/projeto-integrador-II/seriesetv/lancamentos.php" class="dropdown-item text-decoration-none text-light">Lançamentos</a></li>
+                <li><a href="/projeto-integrador-II/seriesetv/criticas.php" class="dropdown-item text-decoration-none text-light">Criticas</a></li>
+                <li><a href="/projeto-integrador-II/seriesetv/noticias.php" class="dropdown-item text-decoration-none text-light">Notícias</a></li>
+                <li><a href="/projeto-integrador-II/seriesetv/atores.php" class="dropdown-item text-decoration-none text-light">Atores</a></li>
             </ul>
         </div>
         <div class="col-3 col-lg-1">
@@ -41,33 +41,33 @@
             <p class="text-center" id="participantes">Participantes</p>
             <div class="row">
                 <div class="col-3 col-lg-3">
-                    <a href="/projeto-integrador-acessivel/quemsomos.php?#gilberto" class="text-decoration-none text-light">
+                    <a href="/projeto-integrador-II/quemsomos.php?#gilberto" class="text-decoration-none text-light">
                         <figure class="fotoParticipante">
-                                <img src="/projeto-integrador-acessivel/img/IMG-20221204-WA0101.jpg" alt="Aqui temos a Imagem do gilberto" class="img-fluid">
+                                <img src="/projeto-integrador-II/img/IMG-20221204-WA0101.jpg" alt="Aqui temos a Imagem do gilberto" class="img-fluid">
                                 <figcaption class="text-center">Gilberto</figcaption>
                         </figure>
                     </a>
                 </div>
                 <div class="col-3 col-lg-3 ">
-                    <a href="/projeto-integrador-acessivel/quemsomos.php?#joseguilherme" class="text-decoration-none text-light">
+                    <a href="/projeto-integrador-II/quemsomos.php?#joseguilherme" class="text-decoration-none text-light">
                         <figure class="fotoParticipante">
-                                <img src="/projeto-integrador-acessivel/img/IMG_20220922_071956875.jpg" class="img-fluid" alt="Aqui temos a Imagem do José">
+                                <img src="/projeto-integrador-II/img/IMG_20220922_071956875.jpg" class="img-fluid" alt="Aqui temos a Imagem do José">
                                 <figcaption class="text-center">José Guilherme</figcaption>
                         </figure>
                     </a>
                 </div>
                 <div class="col-3 col-lg-3">
-                    <a href="/projeto-integrador-acessivel/quemsomos.php?#melissa" class="text-decoration-none text-light">
+                    <a href="/projeto-integrador-II/quemsomos.php?#melissa" class="text-decoration-none text-light">
                         <figure class="fotoParticipante">
-                                <img src="/projeto-integrador-acessivel/img/Screenshot_20221204-094744.png" class="img-fluid" alt='Aqui temos a imagem da Melissa'>
+                                <img src="/projeto-integrador-II/img/Screenshot_20221204-094744.png" class="img-fluid" alt='Aqui temos a imagem da Melissa'>
                                 <figcaption class="text-center">Melissa</figcaption>
                         </figure>
                     </a>
                 </div>
                 <div class="col-3 col-lg-3">
-                    <a href="/projeto-integrador-acessivel/quemsomos.php?#yasmin" class="text-decoration-none text-light">
+                    <a href="/projeto-integrador-II/quemsomos.php?#yasmin" class="text-decoration-none text-light">
                         <figure class="fotoParticipante">
-                            <img src="/projeto-integrador-acessivel/img/IMG-20221204-WA0097.jpg" class="img-fluid" alt ="Aqui temos a imagem da Yasmin">
+                            <img src="/projeto-integrador-II/img/IMG-20221204-WA0097.jpg" class="img-fluid" alt ="Aqui temos a imagem da Yasmin">
                             <figcaption class="text-center" >Yasmin</figcaption>
                         </figure>
                     </a>
@@ -84,7 +84,7 @@
         </div>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    <script src="/projeto-integrador-acessivel/js/tabindex.js"></script>
+    <script src="/projeto-integrador-II/js/tabindex.js"></script>
     <script src="js/contato.js"></script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -7,30 +7,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="/projeto-integrador-acessivel/css/tema.css">
+    <link rel="stylesheet" href="/projeto-integrador-II/css/tema.css">
 </head>
 
 <body>
     <header>
         <nav class="navbar navbar-expand-lg  sticky-top border">
             <div class="container" >
-                <a class="navbar-brand" href="/projeto-integrador-acessivel/index.php" tabindex="0"><span class="cor-letra-home">PM</span>A&M</a>
+                <a class="navbar-brand" href="/projeto-integrador-II/index.php" tabindex="0"><span class="cor-letra-home">PM</span>A&M</a>
                 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="/projeto-integrador-acessivel/index.php" accesskey="h" tabindex="1" id="home">Home</a>
+                            <a class="nav-link active" aria-current="page" href="/projeto-integrador-II/index.php" accesskey="h" tabindex="1" id="home">Home</a>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false" accesskey="c" tabindex="2">
                                 Cinema
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="/projeto-integrador-acessivel/cinema/indexcinema.php">Cinema</a></li>
-                                <li><a class="dropdown-item" href="/projeto-integrador-acessivel/cinema/filmes.php">Filmes</a></li>
-                                <li><a class="dropdown-item" href="/projeto-integrador-acessivel/cinema/streaming.php">Streaming</a></li>
-                                <li><a class="dropdown-item" href="/projeto-integrador-acessivel/cinema/literatura.php">Literatura</a></li>
-                                <li><a class="dropdown-item" href="/projeto-integrador-acessivel/cinema/entrevistas.php">Entrevista</a></li>
+                                <li><a class="dropdown-item" href="/projeto-integrador-II/cinema/indexcinema.php">Cinema</a></li>
+                                <li><a class="dropdown-item" href="/projeto-integrador-II/cinema/filmes.php">Filmes</a></li>
+                                <li><a class="dropdown-item" href="/projeto-integrador-II/cinema/streaming.php">Streaming</a></li>
+                                <li><a class="dropdown-item" href="/projeto-integrador-II/cinema/literatura.php">Literatura</a></li>
+                                <li><a class="dropdown-item" href="/projeto-integrador-II/cinema/entrevistas.php">Entrevista</a></li>
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
@@ -38,12 +38,12 @@
                                 HQS
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="/projeto-integrador-acessivel/hqs/indexhqs.php">HQS</a></li>
-                                <li><a class="dropdown-item" href="/projeto-integrador-acessivel/hqs/livros.php">Top Five</a></li>
-                                <li><a class="dropdown-item" href="/projeto-integrador-acessivel/hqs/melhorescriticas.php">Melhores criticas</a></li>
-                                 <li><a class="dropdown-item" href="/projeto-integrador-acessivel/hqs/ultimasnoticias.php">Noticias</a></li>
-                                <li><a class="dropdown-item" href="/projeto-integrador-acessivel/hqs/noticias.php">Materia</a></li>
-                                <li><a class="dropdown-item" href="/projeto-integrador-acessivel/hqs/melhoresautores.php">Melhores autores</a></li>
+                                <li><a class="dropdown-item" href="/projeto-integrador-II/hqs/indexhqs.php">HQS</a></li>
+                                <li><a class="dropdown-item" href="/projeto-integrador-II/hqs/livros.php">Top Five</a></li>
+                                <li><a class="dropdown-item" href="/projeto-integrador-II/hqs/melhorescriticas.php">Melhores criticas</a></li>
+                                 <li><a class="dropdown-item" href="/projeto-integrador-II/hqs/ultimasnoticias.php">Noticias</a></li>
+                                <li><a class="dropdown-item" href="/projeto-integrador-II/hqs/noticias.php">Materia</a></li>
+                                <li><a class="dropdown-item" href="/projeto-integrador-II/hqs/melhoresautores.php">Melhores autores</a></li>
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
@@ -51,11 +51,11 @@
                                 Cosplay
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="/projeto-integrador-acessivel/cosplay/cosplay.php">Cosplay</a></li>
-                                <li><a class="dropdown-item" href="/projeto-integrador-acessivel/cosplay/animes.php">Animes</a></li>
-                                <li><a class="dropdown-item" href="/projeto-integrador-acessivel/cosplay/noticias.php">Noticias</a></li>
-                                <li><a class="dropdown-item" href="/projeto-integrador-acessivel/cosplay/eventos.php">Eventos</a></li>
-                                <li><a class="dropdown-item" href="/projeto-integrador-acessivel/cosplay/games.php">Games</a></li>
+                                <li><a class="dropdown-item" href="/projeto-integrador-II/cosplay/cosplay.php">Cosplay</a></li>
+                                <li><a class="dropdown-item" href="/projeto-integrador-II/cosplay/animes.php">Animes</a></li>
+                                <li><a class="dropdown-item" href="/projeto-integrador-II/cosplay/noticias.php">Noticias</a></li>
+                                <li><a class="dropdown-item" href="/projeto-integrador-II/cosplay/eventos.php">Eventos</a></li>
+                                <li><a class="dropdown-item" href="/projeto-integrador-II/cosplay/games.php">Games</a></li>
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
@@ -63,28 +63,28 @@
                                 Series e TV
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="/projeto-integrador-acessivel/seriesetv/home.php">Series e Tv</a></li>
-                                <li><a class="dropdown-item" href="/projeto-integrador-acessivel/seriesetv/resenhas.php">Resenhas</a></li>
-                                <li><a class="dropdown-item" href="/projeto-integrador-acessivel/seriesetv/lancamentos.php">Lançamentos</a></li>
-                                <li><a class="dropdown-item" href="/projeto-integrador-acessivel/seriesetv/criticas.php">Criticas</a></li>
-                                <li><a class="dropdown-item" href="/projeto-integrador-acessivel/seriesetv/noticias.php">Notícias</a></li>
-                                <li><a class="dropdown-item" href="/projeto-integrador-acessivel/seriesetv/atores.php">Atores</a></li>
+                                <li><a class="dropdown-item" href="/projeto-integrador-II/seriesetv/home.php">Series e Tv</a></li>
+                                <li><a class="dropdown-item" href="/projeto-integrador-II/seriesetv/resenhas.php">Resenhas</a></li>
+                                <li><a class="dropdown-item" href="/projeto-integrador-II/seriesetv/lancamentos.php">Lançamentos</a></li>
+                                <li><a class="dropdown-item" href="/projeto-integrador-II/seriesetv/criticas.php">Criticas</a></li>
+                                <li><a class="dropdown-item" href="/projeto-integrador-II/seriesetv/noticias.php">Notícias</a></li>
+                                <li><a class="dropdown-item" href="/projeto-integrador-II/seriesetv/atores.php">Atores</a></li>
                             </ul>
                         </li>
                     </ul>
                     <div class="collapse navbar-collapse" id="navbarNavDropdown">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link" href="/projeto-integrador-acessivel/quemsomos.php" accesskey="u" tabindex="6">Quem somos</a>
+                                <a class="nav-link" href="/projeto-integrador-II/quemsomos.php" accesskey="u" tabindex="6">Quem somos</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/projeto-integrador-acessivel/contato.php" accesskey="t" tabindex="7">Contatos</a>
+                                <a class="nav-link" href="/projeto-integrador-II/contato.php" accesskey="t" tabindex="7">Contatos</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/projeto-integrador-acessivel/normasdeutilizacao.php" accesskey="r" tabindex="8">Normas de Utilização</a>
+                                <a class="nav-link" href="/projeto-integrador-II/normasdeutilizacao.php" accesskey="r" tabindex="8">Normas de Utilização</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/projeto-integrador-acessivel/autores.php" accesskey="a" tabindex="9">Referências</a>
+                                <a class="nav-link" href="/projeto-integrador-II/autores.php" accesskey="a" tabindex="9">Referências</a>
                             </li>
                         </ul>
                     </div>
